6c518a3135cc3993d81c2d2848ece4cbb3c472c6
Oliver Haufe <oliver.haufe.git@sos-berlin.com>
2018-06-01 14:15:58 +0200

    update pojos - JobScheduler2

--- pojogen/sos-raml/src/main/resources/com/sos/jobscheduler/model/instruction/IInstruction.java
+++ pojogen/sos-raml/src/main/resources/com/sos/jobscheduler/model/instruction/IInstruction.java
@@ -1,5 +1,17 @@
 package com.sos.jobscheduler.model.instruction;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(
+		use = JsonTypeInfo.Id.NAME, 
+		include = JsonTypeInfo.As.PROPERTY, 
+		property = "TYPE")
+@JsonSubTypes({ 
+		@JsonSubTypes.Type(value = com.sos.jobscheduler.model.instruction.IfElse.class, name = "If"),
+		@JsonSubTypes.Type(value = com.sos.jobscheduler.model.instruction.Job.class, name = "Job"),
+		@JsonSubTypes.Type(value = com.sos.jobscheduler.model.instruction.Workflow.class, name = "Workflow"),
+		@JsonSubTypes.Type(value = com.sos.jobscheduler.model.instruction.ForkJoin.class, name = "ForkJoin") })
 public interface IInstruction {
 	
 	public InstructionType getTYPE();
