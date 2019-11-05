c5715a9266932da62b47f792dd53e56eaebcabe4
Santiago Aucejo <santiago.aucejo.petzoldt.git@sos-berlin.com>
2019-11-05 07:50:58 +0100

    deployment

--- sos-webservices/sos-webservices-joc/src/main/java/com/sos/joc/deploy/mapper/JSObjectDBItemMapper.java
+++ sos-webservices/sos-webservices-joc/src/main/java/com/sos/joc/deploy/mapper/JSObjectDBItemMapper.java
@@ -1,17 +1,28 @@
 package com.sos.joc.deploy.mapper;
 
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sos.jobscheduler.db.inventory.DBItemJSObject;
+import com.sos.jobscheduler.model.agent.AgentRef;
+import com.sos.jobscheduler.model.deploy.DeployType;
+import com.sos.jobscheduler.model.workflow.Workflow;
+import com.sos.joc.model.deploy.IJSObject;
 import com.sos.joc.model.deploy.JSObject;
 
 public class JSObjectDBItemMapper {
 
-	public static DBItemJSObject mapJsObjectToDBitem (final JSObject jsObject) {
+	public static DBItemJSObject mapJsObjectToDBitem (final JSObject jsObject) throws JsonProcessingException {
+		ObjectMapper om = UpDownloadMapper.initiateObjectMapper();
 		DBItemJSObject dbItem = new DBItemJSObject();
 		dbItem.setComment(jsObject.getComment());
-		dbItem.setContent(jsObject.getContent());
+		dbItem.setContent(om.writeValueAsString(jsObject));
 		dbItem.setEditAccount(jsObject.getEditAccount());
 		dbItem.setModified(jsObject.getModified());
-		dbItem.setObjectType(jsObject.getObjectType());
+		dbItem.setObjectType(jsObject.getTYPE().value());
 		dbItem.setParentVersion(jsObject.getParentVersion());
 		dbItem.setPath(jsObject.getPath());
 		dbItem.setPublishAccount(jsObject.getPublishAccount());
@@ -28,13 +39,19 @@ public class JSObjectDBItemMapper {
 		return dbItem;
 	}
 
-	public static JSObject mapDBitemToJsObject (final DBItemJSObject dbItem) {
+	public static JSObject mapDBitemToJsObject (final DBItemJSObject dbItem) throws JsonParseException, JsonMappingException,
+		IOException {
+		ObjectMapper om = UpDownloadMapper.initiateObjectMapper();
 		JSObject jsObject = new JSObject();
 		jsObject.setComment(dbItem.getComment());
-		jsObject.setContent(dbItem.getContent());
 		jsObject.setEditAccount(dbItem.getEditAccount());
 		jsObject.setModified(dbItem.getModified());
-		jsObject.setObjectType(dbItem.getObjectType());
+		jsObject.setTYPE(DeployType.fromValue(dbItem.getObjectType()));
+		if(DeployType.WORKFLOW.equals(jsObject.getTYPE())) {
+			jsObject.setContent(om.readValue(dbItem.getContent(), Workflow.class));
+		} else if (DeployType.AGENT_REF.equals(jsObject.getTYPE())) {
+			jsObject.setContent(om.readValue(dbItem.getContent(), AgentRef.class));
+		}
 		jsObject.setParentVersion(dbItem.getParentVersion());
 		jsObject.setPath(dbItem.getPath());
 		jsObject.setPublishAccount(dbItem.getPublishAccount());
