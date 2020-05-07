87f69f7dc2b1c2533127a8ffbdd42af510fb61cf
Santiago Aucejo <santiago.aucejo.petzoldt.git@sos-berlin.com>
2020-05-07 08:22:24 +0200

    JS2-48:
    - DBItem changed to reflect table changes

--- sos-jobscheduler/sos-jobscheduler-db/src/main/java/com/sos/jobscheduler/db/inventory/DBItemJSConfigToSchedulerMapping.java
+++ sos-jobscheduler/sos-jobscheduler-db/src/main/java/com/sos/jobscheduler/db/inventory/DBItemJSConfigToSchedulerMapping.java
@@ -4,38 +4,36 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
 
 import com.sos.jobscheduler.db.DBItem;
 import com.sos.jobscheduler.db.DBLayer;
 
 @Entity
-@Table( name = DBLayer.TABLE_JS_CONFIG_TO_SCHEDULER_MAPPING, 
-		uniqueConstraints = { @UniqueConstraint(columnNames = { "[CONFIGURATION_ID]", "[SCHEDULER_ID]" }) })
+@Table( name = DBLayer.TABLE_JS_CONFIG_TO_SCHEDULER_MAPPING)
 public class DBItemJSConfigToSchedulerMapping extends DBItem {
 
 	private static final long serialVersionUID = 1L;
+
+    @Column(name = "[SCHEDULER_ID]", nullable = false)
+    @Id
+    private String schedulerId;
 
     @Column(name = "[CONFIGURATION_ID]", nullable = false)
     @Id
     private Long configurationId;
 
-    @Column(name = "[SCHEDULER_ID]", nullable = false)
-    @Id
-    private String schedulerId;
+    public String getSchedulerId() {
+        return schedulerId;
+    }
+    public void setSchedulerId(String schedulerId) {
+        this.schedulerId = schedulerId;
+    }
 
 	public Long getConfigurationId() {
 		return configurationId;
 	}
 	public void setConfigurationId(Long val) {
 		this.configurationId = val;
-	}
-
-    public String getSchedulerId() {
-		return schedulerId;
-	}
-	public void setSchedulerId(String schedulerId) {
-		this.schedulerId = schedulerId;
 	}
 
 }
