721ef30c3284af3bf93e60cf61ef2001f29a1353
Oliver Haufe <oliver.haufe.git@sos-berlin.com>
2020-06-17 12:53:20 +0200

    java prefs should be silent

--- sos-webservices/sos-webservices-joc-global/src/main/java/com/sos/joc/classes/JOCPreferences.java
+++ sos-webservices/sos-webservices-joc-global/src/main/java/com/sos/joc/classes/JOCPreferences.java
@@ -1,5 +1,7 @@
 package com.sos.joc.classes;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.prefs.Preferences;
 
 public class JOCPreferences {
@@ -9,10 +11,16 @@ public class JOCPreferences {
     public JOCPreferences(String userName) {
         prefs = Preferences.userNodeForPackage(this.getClass());
         this.userName = userName;
+        Logger l = Logger.getLogger("java.util.prefs");
+        l.setLevel(Level.OFF);
     }
 
     public void put(String key, String value){
-        prefs.node(WebserviceConstants.JOC_COCKPIT).put(getKey(key),value);
+        try {
+            prefs.node(WebserviceConstants.JOC_COCKPIT).put(getKey(key),value);
+            prefs.node(WebserviceConstants.JOC_COCKPIT).flush();
+        } catch (Exception e) {
+        }
     }
     public String get(String key, String defaultValue){
         return prefs.node(WebserviceConstants.JOC_COCKPIT).get(getKey(key),defaultValue);
