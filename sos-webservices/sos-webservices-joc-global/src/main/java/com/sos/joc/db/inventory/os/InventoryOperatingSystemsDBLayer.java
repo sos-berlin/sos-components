10c7e5913c02ab39640159c4c5d5528321917af6
SOS-RE <robert.ehrlich@sos-berlin.com>
2020-07-07 17:29:26 +0200

    JS7-161 refactoring

--- sos-webservices/sos-webservices-joc-global/src/main/java/com/sos/joc/db/inventory/os/InventoryOperatingSystemsDBLayer.java
+++ sos-webservices/sos-webservices-joc-global/src/main/java/com/sos/joc/db/inventory/os/InventoryOperatingSystemsDBLayer.java
@@ -16,8 +16,8 @@ import com.sos.joc.exceptions.DBConnectionRefusedException;
 import com.sos.joc.exceptions.DBInvalidDataException;
 
 public class InventoryOperatingSystemsDBLayer {
-	
-	private SOSHibernateSession session;
+
+    private SOSHibernateSession session;
 
     public InventoryOperatingSystemsDBLayer(SOSHibernateSession connection) {
         this.session = connection;
@@ -25,35 +25,33 @@ public class InventoryOperatingSystemsDBLayer {
 
     public DBItemInventoryOperatingSystem getInventoryOperatingSystem(Long osId) throws DBInvalidDataException, DBConnectionRefusedException {
         try {
-        	if (osId == null || osId == 0L) {
-        		return null;
-        	}
-        	return session.get(DBItemInventoryOperatingSystem.class, osId);
+            if (osId == null || osId == 0L) {
+                return null;
+            }
+            return session.get(DBItemInventoryOperatingSystem.class, osId);
         } catch (SOSHibernateInvalidSessionException ex) {
             throw new DBConnectionRefusedException(ex);
         } catch (Exception ex) {
             throw new DBInvalidDataException(ex);
         }
     }
-    
-    public DBItemInventoryOperatingSystem getOSItem(String hostname)
-			throws DBConnectionRefusedException, DBInvalidDataException {
-		try {
-			StringBuilder sql = new StringBuilder();
-			sql.append("from ").append(DBLayer.DBITEM_INV_OPERATING_SYSTEMS);
-			sql.append(" where hostname = :hostname");
-			Query<DBItemInventoryOperatingSystem> query = session.createQuery(sql.toString());
-			query.setParameter("hostname", hostname);
-			return session.getSingleResult(query);
-		} catch (SOSHibernateInvalidSessionException ex) {
-			throw new DBConnectionRefusedException(ex);
-		} catch (Exception ex) {
-			throw new DBInvalidDataException(ex);
-		}
-	}
-    
-    public List<DBItemInventoryOperatingSystem> getOSItems(Collection<Long> ids)
-            throws DBConnectionRefusedException, DBInvalidDataException {
+
+    public DBItemInventoryOperatingSystem getOSItem(String hostname) throws DBConnectionRefusedException, DBInvalidDataException {
+        try {
+            StringBuilder sql = new StringBuilder();
+            sql.append("from ").append(DBLayer.DBITEM_INV_OPERATING_SYSTEMS);
+            sql.append(" where hostname = :hostname");
+            Query<DBItemInventoryOperatingSystem> query = session.createQuery(sql.toString());
+            query.setParameter("hostname", hostname);
+            return session.getSingleResult(query);
+        } catch (SOSHibernateInvalidSessionException ex) {
+            throw new DBConnectionRefusedException(ex);
+        } catch (Exception ex) {
+            throw new DBInvalidDataException(ex);
+        }
+    }
+
+    public List<DBItemInventoryOperatingSystem> getOSItems(Collection<Long> ids) throws DBConnectionRefusedException, DBInvalidDataException {
         try {
             if (ids == null || ids.isEmpty()) {
                 return null;
@@ -70,37 +68,36 @@ public class InventoryOperatingSystemsDBLayer {
             throw new DBInvalidDataException(ex);
         }
     }
-	
-	public Long saveOrUpdateOSItem(DBItemInventoryOperatingSystem osItem) throws DBConnectionRefusedException, DBInvalidDataException {
-		try {
-			if (osItem == null || osItem.getHostname() == null || osItem.getHostname().isEmpty()) {
-				return 0L;
-			}
-			DBItemInventoryOperatingSystem oldOsItem = getOSItem(osItem.getHostname());
-			if (oldOsItem == null) {
-				osItem.setModified(Date.from(Instant.now()));
-				session.save(osItem);
-				return osItem.getId();
-			} else {
-				EqualsBuilder eb = new EqualsBuilder();
-				eb.append(oldOsItem.getArchitecture(), osItem.getArchitecture())
-					.append(oldOsItem.getDistribution(), osItem.getDistribution())
-					.append(oldOsItem.getName(), osItem.getName());
-				if(!eb.isEquals()) {
-					osItem.setId(oldOsItem.getId());
-					osItem.setModified(Date.from(Instant.now()));
-					session.update(osItem);
-				}
-				return oldOsItem.getId();
-			}
-		} catch (SOSHibernateInvalidSessionException ex) {
-			throw new DBConnectionRefusedException(ex);
-		} catch (Exception ex) {
-			throw new DBInvalidDataException(ex);
-		}
-	}
-	
-	public void deleteOSItem(DBItemInventoryOperatingSystem osItem) throws DBConnectionRefusedException, DBInvalidDataException {
+
+    public Long saveOrUpdateOSItem(DBItemInventoryOperatingSystem osItem) throws DBConnectionRefusedException, DBInvalidDataException {
+        try {
+            if (osItem == null || osItem.getHostname() == null || osItem.getHostname().isEmpty()) {
+                return 0L;
+            }
+            DBItemInventoryOperatingSystem oldOsItem = getOSItem(osItem.getHostname());
+            if (oldOsItem == null) {
+                osItem.setModified(Date.from(Instant.now()));
+                session.save(osItem);
+                return osItem.getId();
+            } else {
+                EqualsBuilder eb = new EqualsBuilder();
+                eb.append(oldOsItem.getArchitecture(), osItem.getArchitecture()).append(oldOsItem.getDistribution(), osItem.getDistribution()).append(
+                        oldOsItem.getName(), osItem.getName());
+                if (!eb.isEquals()) {
+                    osItem.setId(oldOsItem.getId());
+                    osItem.setModified(Date.from(Instant.now()));
+                    session.update(osItem);
+                }
+                return oldOsItem.getId();
+            }
+        } catch (SOSHibernateInvalidSessionException ex) {
+            throw new DBConnectionRefusedException(ex);
+        } catch (Exception ex) {
+            throw new DBInvalidDataException(ex);
+        }
+    }
+
+    public void deleteOSItem(DBItemInventoryOperatingSystem osItem) throws DBConnectionRefusedException, DBInvalidDataException {
         try {
             if (osItem != null) {
                 session.delete(osItem);
