86a4871a5cee078550b0f2c28952ac3ad6d2c4ff
Oliver Haufe <oliver.haufe.git@sos-berlin.com>
2020-07-30 12:04:22 +0200

    JS7-14

--- sos-webservices/sos-webservices-joc/src/main/java/com/sos/joc/orders/impl/OrdersResourceOverviewSnapshotImpl.java
+++ sos-webservices/sos-webservices-joc/src/main/java/com/sos/joc/orders/impl/OrdersResourceOverviewSnapshotImpl.java
@@ -1,14 +1,16 @@
 package com.sos.joc.orders.impl;
 
 import java.time.Instant;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import javax.ws.rs.Path;
 
@@ -27,25 +29,20 @@ import com.sos.joc.orders.resource.IOrdersResourceOverviewSnapshot;
 import com.sos.schema.JsonValidator;
 
 import js7.data.order.Order;
-import js7.data.workflow.WorkflowPath;
-import js7.proxy.javaapi.JControllerProxy;
 import js7.proxy.javaapi.data.JControllerState;
+import js7.proxy.javaapi.data.JOrder;
 import js7.proxy.javaapi.data.JOrderPredicates;
 
 @Path("orders")
 public class OrdersResourceOverviewSnapshotImpl extends JOCResourceImpl implements IOrdersResourceOverviewSnapshot {
 
     private static final String API_CALL = "./orders/overview/snapshot";
-    private static final List<String> groupStates = Arrays.asList("pending", "waiting", "blocked", "suspended", "failed", "running");
     private static final Map<Class<? extends Order.State>, String> groupStatesMap = Collections.unmodifiableMap(
             new HashMap<Class<? extends Order.State>, String>() {
 
                 /*
-                 * +PENDING: Fresh 
-                 * +WAITING: Forked, Offering, Awaiting, DelayedAfterError 
-                 * -BLOCKED: Fresh late +RUNNING: Ready, Processing, Processed
-                 * ---FAILED: Failed, FailedWhileFresh, FailedInFork, Broken 
-                 * --SUSPENDED any state+Suspended Annotation
+                 * +PENDING: Fresh +WAITING: Forked, Offering, Awaiting, DelayedAfterError -BLOCKED: Fresh late +RUNNING: Ready, Processing, Processed
+                 * ---FAILED: Failed, FailedWhileFresh, FailedInFork, Broken --SUSPENDED any state+Suspended Annotation
                  */
                 private static final long serialVersionUID = 1L;
 
@@ -70,18 +67,20 @@ public class OrdersResourceOverviewSnapshotImpl extends JOCResourceImpl implemen
 
     @Override
     public JOCDefaultResponse postOrdersOverviewSnapshot(String accessToken, byte[] filterBytes) {
-		try {
-		    JsonValidator.validateFailFast(filterBytes, JobSchedulerId.class);
-		    WorkflowsFilter body = Globals.objectMapper.readValue(filterBytes, WorkflowsFilter.class);
-		    
-		    JOCDefaultResponse jocDefaultResponse = init(API_CALL, body, accessToken, body.getJobschedulerId(),
-                    getPermissonsJocCockpit(body.getJobschedulerId(), accessToken).getOrder().getView().isStatus());
+        try {
+            JsonValidator.validateFailFast(filterBytes, JobSchedulerId.class);
+            WorkflowsFilter body = Globals.objectMapper.readValue(filterBytes, WorkflowsFilter.class);
+
+            JOCDefaultResponse jocDefaultResponse = init(API_CALL, body, accessToken, body.getJobschedulerId(), getPermissonsJocCockpit(body
+                    .getJobschedulerId(), accessToken).getOrder().getView().isStatus());
             if (jocDefaultResponse != null) {
                 return jocDefaultResponse;
             }
             
-            JControllerProxy controllerProxy = Proxy.of(this.getUrl());
-            return JOCDefaultResponse.responseStatus200(getSnapshot(controllerProxy.currentState(), body.getWorkflows(), folderPermissions.getListOfFolders()));
+            boolean withWorkFlowFilter = body.getWorkflows() != null && !body.getWorkflows().isEmpty();
+
+            return JOCDefaultResponse.responseStatus200(getSnapshot(Proxy.of(this.getUrl()).currentState(), checkFolderPermission(body.getWorkflows(),
+                    folderPermissions.getListOfFolders()), withWorkFlowFilter));
 
         } catch (JobSchedulerConnectionResetException e) {
             e.addErrorMetaInfo(getJocError());
@@ -94,39 +93,88 @@ public class OrdersResourceOverviewSnapshotImpl extends JOCResourceImpl implemen
         }
 
     }
-    
-    private static OrdersSnapshot getSnapshot(JControllerState controllerState, List<String> workflowPaths, Set<Folder> permittedFolders) throws Exception {
-        
-        Map<Class<? extends Order.State>, Integer> orderStates = null;
-        if (!workflowPaths.isEmpty()) {
-            if (workflowPaths.size() == 1) {
-                orderStates = controllerState.orderStateToCount(JOrderPredicates.byWorkflowPath(WorkflowPath.of(workflowPaths.get(0))));
+
+    private static Set<String> checkFolderPermission(List<String> _workflowPaths, Set<Folder> permittedFolders) {
+        Set<String> workflowPaths = new HashSet<>();
+        if (_workflowPaths != null) {
+            if (permittedFolders != null && !permittedFolders.isEmpty()) {
+                workflowPaths = _workflowPaths.stream().filter(w -> folderIsPermitted(w, permittedFolders)).collect(Collectors.toSet());
             } else {
+                workflowPaths = _workflowPaths.stream().collect(Collectors.toSet());
+            }
+        }
+        return workflowPaths;
+    }
+
+    private static OrdersSnapshot getSnapshot(JControllerState controllerState, Set<String> workflowPaths, boolean withWorkFlowFilter) {
+
+        final long nowMillis = controllerState.eventId() / 1000;
+        final Instant now = Instant.ofEpochMilli(nowMillis);
+        Map<Class<? extends Order.State>, Integer> orderStates = null;
+        Stream<JOrder> blockedOrders = null;
+
+        if (withWorkFlowFilter) {
+            if (!workflowPaths.isEmpty()) {
                 orderStates = controllerState.orderStateToCount(o -> workflowPaths.contains(o.workflowId().path().string()));
+                if (orderStates.containsKey(Order.Fresh.class) && orderStates.get(Order.Fresh.class) > 0) {
+                    blockedOrders = controllerState.ordersBy(JOrderPredicates.byOrderState(Order.Fresh.class)).filter(o -> workflowPaths.contains(o
+                            .workflowId().path().string()));
+                }
+            } else {
+                // no folder permissions
+                orderStates = new HashMap<Class<? extends Order.State>, Integer>();
             }
         } else {
             orderStates = controllerState.orderStateToCount();
+            if (orderStates.containsKey(Order.Fresh.class) && orderStates.get(Order.Fresh.class) > 0) {
+                blockedOrders = controllerState.ordersBy(JOrderPredicates.byOrderState(Order.Fresh.class));
+            }
+        }
+
+        int numOfBlockedOrders = 0;
+        if (blockedOrders != null) {
+            // numOfBlockedOrders = blockedOrders.map(o -> {
+            // try {
+            // OrderItem item = Globals.objectMapper.readValue(o.toJson(), OrderItem.class);
+            // if (item.getState().getScheduledFor() != null) {
+            // return item.getState().getScheduledFor();
+            // } else {
+            // return null;
+            // }
+            // } catch (Exception e1) {
+            // return null;
+            // }
+            // }).filter(tstamp -> tstamp != null && tstamp < nowMillis).mapToInt(e -> 1).sum();
+
+            numOfBlockedOrders = blockedOrders.map(order -> order.underlying().state().maybeDelayedUntil()).filter(tstamp -> !tstamp.isEmpty()
+                    && tstamp.get().toInstant().isBefore(now)).mapToInt(item -> 1).sum();
         }
 
         final Map<String, Integer> map = orderStates.entrySet().stream().collect(Collectors.groupingBy(entry -> groupStatesMap.get(entry.getKey()),
                 Collectors.summingInt(entry -> entry.getValue())));
-        groupStates.stream().forEach(state -> map.putIfAbsent(state, 0));
-        
-        //TODO blocked comes from DailyPlan
-        //TODO suspended is not yet supported
+        map.put("blocked", numOfBlockedOrders);
+        groupStatesMap.values().stream().distinct().forEach(state -> map.putIfAbsent(state, 0));
+
+        // TODO suspended is not yet supported
 
         OrdersSummary summary = new OrdersSummary();
         summary.setBlocked(map.get("blocked"));
-        summary.setPending(map.get("pending"));
+        summary.setPending(map.get("pending") - map.get("blocked"));
         summary.setRunning(map.get("running"));
         summary.setFailed(map.get("failed"));
         summary.setSuspended(map.get("suspended"));
         summary.setWaiting(map.get("waiting"));
-        
+
         OrdersSnapshot entity = new OrdersSnapshot();
-        entity.setSurveyDate(Date.from(Instant.ofEpochMilli(controllerState.eventId() / 1000)));
+        entity.setSurveyDate(Date.from(now));
         entity.setOrders(summary);
         entity.setDeliveryDate(Date.from(Instant.now()));
         return entity;
+    }
+
+    private static boolean folderIsPermitted(String folder, Set<Folder> listOfFolders) {
+        Predicate<Folder> filter = f -> f.getFolder().equals(folder) || (f.getRecursive() && ("/".equals(f.getFolder()) || folder.startsWith(f
+                .getFolder() + "/")));
+        return listOfFolders.stream().parallel().anyMatch(filter);
     }
 }
