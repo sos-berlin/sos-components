e48654b007c382230d02bbe0eff45637be561c8d
Oliver Haufe <oliver.haufe.git@sos-berlin.com>
2020-08-14 13:16:53 +0200

    Grizzly with Proxies

---
+++ sos-webservices/sos-webservices-server/src/main/java/com/sos/joc2/JocServletContainer.java
@@ -0,0 +1,53 @@
+package com.sos.joc2;
+
+import javax.servlet.ServletException;
+
+import org.glassfish.jersey.servlet.ServletContainer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.sos.commons.util.SOSShell;
+import com.sos.joc.Globals;
+import com.sos.joc.classes.JocCockpitProperties;
+import com.sos.joc.classes.cluster.JocClusterService;
+import com.sos.joc.classes.proxy.Proxies;
+import com.sos.joc.classes.proxy.ProxyUser;
+
+public class JocServletContainer extends ServletContainer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(JocServletContainer.class);
+
+    private static final long serialVersionUID = 1L;
+
+    public JocServletContainer() {
+        super();
+    }
+
+    @Override
+    public void init() throws ServletException {
+        LOGGER.debug("----> init on starting JOC");
+        super.init();
+
+        Globals.sosCockpitProperties = new JocCockpitProperties();
+        Proxies.getInstance().startAll(Globals.sosCockpitProperties, ProxyUser.JOC);
+        SOSShell.printSystemInfos();
+        SOSShell.printJVMInfos();
+
+        //JocClusterService.getInstance().start();
+    }
+
+    @Override
+    public void destroy() {
+        LOGGER.debug("----> destroy on close JOC");
+        super.destroy();
+
+        Proxies.getInstance().closeAll();
+        //JocClusterService.getInstance().stop(true);
+
+        if (Globals.sosHibernateFactory != null) {
+            LOGGER.info("----> closing DB Connections");
+            Globals.sosHibernateFactory.close();
+        }
+    }
+
+}
--- sos-webservices/sos-webservices-server/src/main/java/com/sos/joc2/Main.java
+++ sos-webservices/sos-webservices-server/src/main/java/com/sos/joc2/Main.java
@@ -6,15 +6,18 @@ import java.util.logging.ConsoleHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.servlet.ServletRegistration;
+
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
-import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
+import org.glassfish.jersey.server.ResourceConfig;
 
  
 public class Main {
     // Base URI the Grizzly HTTP server will listen on
-    public static final String BASE_URI = "http://localhost:8090/rest/";
+    private static final String BASE_URI = "http://localhost:8090/rest/";
+    
 
     /**
      * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
@@ -23,11 +26,17 @@ public class Main {
     public static HttpServer startServer() {
         // create a resource config that scans for JAX-RS resources and providers
         // in com.sos.webservices package
-       final ResourceConfig rc = new ResourceConfig().packages("com.sos.webservices","com.sos.joc","com.sos.auth").register(MultiPartFeature.class);
+        final ResourceConfig rc = new ResourceConfig().packages("com.sos.webservices", "com.sos.joc", "com.sos.auth").register(
+                MultiPartFeature.class);
 
         // create and start a new instance of grizzly http server
         // exposing the Jersey application at BASE_URI
-        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+        HttpServer httpServer = GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+        org.glassfish.grizzly.servlet.WebappContext context = new org.glassfish.grizzly.servlet.WebappContext("WebappContext", "");
+        ServletRegistration registration = context.addServlet("ServletContainer", new JocServletContainer());
+        registration.addMapping("/*");
+        context.deploy(httpServer);
+        return httpServer;
     }
 
     public  static void setLogger() {
