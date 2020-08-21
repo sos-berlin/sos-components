48253da6e2e4921d555bdcbedccab256c6a8eb02
Santiago Aucejo <santiago.aucejo.petzoldt.git@sos-berlin.com>
2020-08-21 08:22:43 +0200

    Entity error fixed for view DEP_CONFIGURATIONS

--- sos-joc/sos-joc-db/src/main/java/com/sos/joc/db/deployment/DBItemDepConfiguration.java
+++ sos-joc/sos-joc-db/src/main/java/com/sos/joc/db/deployment/DBItemDepConfiguration.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -19,6 +20,7 @@ public class DBItemDepConfiguration extends DBItem {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
     @Column(name = "[ID]", nullable = false)
     private Long id;
 
