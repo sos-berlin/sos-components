2d819392714e848a985e730278972fd48c313c4a
SOS-RE <robert.ehrlich@sos-berlin.com>
2020-10-29 14:35:36 +0100

    fat events removed

--- sos-js7/sos-js7-event/src/main/java/com/sos/js7/event/controller/fatevent/FatEventMeta.java
+++
@@ -1,9 +0,0 @@
-package com.sos.js7.event.controller.fatevent;
-
-public final class FatEventMeta {
-
-    public static enum FatEventType {
-        ControllerReadyFat, AgentReadyFat, OrderAddedFat, OrderCancelledFat, OrderFailedFat, OrderFinishedFat, OrderForkedFat, OrderJoinedFat, OrderProcessedFat, OrderProcessingStartedFat, OrderStdoutWrittenFat, OrderStderrWrittenFat
-    };
-
-}
