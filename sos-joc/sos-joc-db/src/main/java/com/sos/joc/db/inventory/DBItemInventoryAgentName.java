722d44d2092c08b9668bc8158f52872a0acd4b54
Oliver Haufe <oliver.haufe.git@sos-berlin.com>
2020-11-23 10:34:21 +0100

    agents api

--- sos-joc/sos-joc-db/src/main/java/com/sos/joc/db/inventory/DBItemInventoryAgentName.java
+++ sos-joc/sos-joc-db/src/main/java/com/sos/joc/db/inventory/DBItemInventoryAgentName.java
@@ -2,21 +2,23 @@ package com.sos.joc.db.inventory;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
 
 import com.sos.joc.db.DBItem;
 import com.sos.joc.db.DBLayer;
 
 @Entity
-@Table(name = DBLayer.TABLE_INV_AGENT_NAMES, uniqueConstraints = { @UniqueConstraint(columnNames = { "[AGENT_ID]", "[AGENT_NAME]" }) })
+@Table(name = DBLayer.TABLE_INV_AGENT_NAMES)
 public class DBItemInventoryAgentName extends DBItem {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
     @Column(name = "[AGENT_ID]", nullable = false)
     private String agentId;
 
+    @Id
     @Column(name = "[AGENT_NAME]", nullable = false)
     private String agentName;
 
