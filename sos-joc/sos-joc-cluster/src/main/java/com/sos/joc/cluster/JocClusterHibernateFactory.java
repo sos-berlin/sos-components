f20efce5f492d47a227d1ff307ee97c323573d73
SOS-RE <robert.ehrlich@sos-berlin.com>
2021-01-26 13:45:43 +0100

    joc cluster - hikari pool

--- sos-joc/sos-joc-cluster/src/main/java/com/sos/joc/cluster/JocClusterHibernateFactory.java
+++ sos-joc/sos-joc-cluster/src/main/java/com/sos/joc/cluster/JocClusterHibernateFactory.java
@@ -23,22 +23,11 @@ public class JocClusterHibernateFactory extends SOSHibernateFactory {
     @Override
     public void adjustConfiguration(Configuration config) {
         if (config.getProperties().getProperty("hibernate.c3p0.maxPoolSize") != null) {
-            config.getProperties().entrySet().removeIf(entry -> entry.getKey().toString().startsWith("hibernate.c3p0"));
-
             config.setProperty("hibernate.c3p0.minPoolSize", String.valueOf(minPool));
             config.setProperty("hibernate.c3p0.maxPoolSize", String.valueOf(maxPool));
             config.setProperty("hibernate.c3p0.initialPoolSize", "1");
-            config.setProperty("hibernate.c3p0.acquireIncrement", "1");
-            config.setProperty("hibernate.c3p0.maxIdleTime", "14400");
-            config.setProperty("hibernate.c3p0.maxConnectionAge", "14400");
-            config.setProperty("hibernate.c3p0.idleConnectionTestPeriod", "16200");
-            config.setProperty("hibernate.c3p0.checkoutTimeout", "0");
-            config.setProperty("hibernate.c3p0.acquireRetryAttempts", "1");
         } else if (config.getProperties().getProperty("hibernate.hikari.maximumPoolSize") != null) {
-            config.getProperties().entrySet().removeIf(entry -> entry.getKey().toString().startsWith("hibernate.hikari"));
-
             config.setProperty("hibernate.hikari.maximumPoolSize", String.valueOf(maxPool));
-            config.setProperty("hibernate.hikari.leakDetectionThreshold", "30000");
         }
     }
 
