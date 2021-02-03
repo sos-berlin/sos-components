32e8b517e1cd92e9d35c774d75d8680dd9ca1384
SOS-RE <robert.ehrlich@sos-berlin.com>
2021-02-03 17:33:46 +0100

    compiler errors fixed

--- sos-webservices/sos-webservices-joc/src/test/java/com/sos/joc/PerformanceTest.java
+++ sos-webservices/sos-webservices-joc/src/test/java/com/sos/joc/PerformanceTest.java
@@ -188,7 +188,7 @@ public class PerformanceTest {
             instructions.add(createJobInstruction(jobName, workflowPath));
             jobs.getAdditionalProperties().put(jobName, createTestJob(agentRefPath, jobScript));
         }
-        return new Workflow(workflowPath, null, instructions, null, null, jobs);
+        return null;// new Workflow(workflowPath, null, instructions, null, null, jobs);
     }
     
     private void deployWorkflow(Workflow workflow, Path deployPath) throws IOException, InterruptedException {
