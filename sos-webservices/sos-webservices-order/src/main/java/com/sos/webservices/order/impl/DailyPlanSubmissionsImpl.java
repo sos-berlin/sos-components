b2249b5970c05ce145a57b79304daadea57ed8e1
uwe-risse-sos <uwe.risse@sos-berlin.com>
2021-03-15 17:13:26 +0100

    Dailyplan Delete

--- sos-webservices/sos-webservices-order/src/main/java/com/sos/webservices/order/impl/DailyPlanSubmissionsImpl.java
+++ sos-webservices/sos-webservices-order/src/main/java/com/sos/webservices/order/impl/DailyPlanSubmissionsImpl.java
@@ -1,26 +1,37 @@
 package com.sos.webservices.order.impl;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.TimeZone;
 
 import javax.ws.rs.Path;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.sos.commons.hibernate.SOSHibernateFactory;
 import com.sos.commons.hibernate.SOSHibernateSession;
 import com.sos.joc.Globals;
 import com.sos.joc.classes.JOCDefaultResponse;
 import com.sos.joc.classes.JOCResourceImpl;
 import com.sos.joc.classes.JobSchedulerDate;
+import com.sos.joc.cluster.JocCluster;
+import com.sos.joc.cluster.configuration.JocClusterConfiguration;
+import com.sos.joc.cluster.configuration.JocConfiguration;
+import com.sos.joc.cluster.configuration.globals.ConfigurationGlobals.DefaultSections;
+import com.sos.joc.cluster.configuration.globals.common.AConfigurationSection;
 import com.sos.joc.db.orders.DBItemDailyPlanSubmissions;
 import com.sos.joc.exceptions.JocException;
 import com.sos.joc.model.dailyplan.DailyPlanSubmissions;
 import com.sos.joc.model.dailyplan.DailyPlanSubmissionsFilter;
 import com.sos.joc.model.dailyplan.DailyPlanSubmissionsItem;
+import com.sos.js7.order.initiator.OrderInitiatorSettings;
+import com.sos.js7.order.initiator.classes.GlobalSettingsReader;
 import com.sos.js7.order.initiator.db.DBLayerDailyPlanSubmissions;
 import com.sos.js7.order.initiator.db.FilterDailyPlanSubmissions;
 import com.sos.schema.JsonValidator;
@@ -31,6 +42,7 @@ public class DailyPlanSubmissionsImpl extends JOCResourceImpl implements IDailyP
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DailyPlanSubmissionsImpl.class);
     private static final String API_CALL = "./daily_plan/submissions";
+    private OrderInitiatorSettings settings;
 
     @Override
     public JOCDefaultResponse postDailyPlanSubmissions(String accessToken, byte[] filterBytes) throws JocException {
@@ -100,7 +112,6 @@ public class DailyPlanSubmissionsImpl extends JOCResourceImpl implements IDailyP
         }
     }
 
-
     @Override
     public JOCDefaultResponse postDeleteDailyPlanSubmissions(String accessToken, byte[] filterBytes) throws JocException {
         SOSHibernateSession sosHibernateSession = null;
@@ -122,6 +133,7 @@ public class DailyPlanSubmissionsImpl extends JOCResourceImpl implements IDailyP
 
             sosHibernateSession = Globals.createSosHibernateStatelessConnection(API_CALL);
 
+            setSettings();
             DBLayerDailyPlanSubmissions dbLayerDailyPlan = new DBLayerDailyPlanSubmissions(sosHibernateSession);
 
             sosHibernateSession.setAutoCommit(false);
@@ -142,25 +154,23 @@ public class DailyPlanSubmissionsImpl extends JOCResourceImpl implements IDailyP
                 calendar.set(java.util.Calendar.SECOND, 0);
                 calendar.set(java.util.Calendar.MILLISECOND, 0);
                 calendar.set(java.util.Calendar.MINUTE, 0);
-                
+
                 filter.setDateFor(calendar.getTime());
-                
 
             } else {
 
                 if (dailyPlanSubmissionHistoryFilter.getFilter().getDateFrom() != null) {
                     Date fromDate = JobSchedulerDate.getDateFrom(dailyPlanSubmissionHistoryFilter.getFilter().getDateFrom(),
                             dailyPlanSubmissionHistoryFilter.getTimeZone());
-                    filter.setDateFrom(fromDate);
+                    filter.setDateFrom(utc2Timezone(fromDate, dailyPlanSubmissionHistoryFilter.getTimeZone()));
                 }
                 if (dailyPlanSubmissionHistoryFilter.getFilter().getDateTo() != null) {
                     Date toDate = JobSchedulerDate.getDateTo(dailyPlanSubmissionHistoryFilter.getFilter().getDateTo(),
                             dailyPlanSubmissionHistoryFilter.getTimeZone());
-                    filter.setDateTo(toDate);
+                    filter.setDateTo(utc2Timezone(toDate, dailyPlanSubmissionHistoryFilter.getTimeZone()));
                 }
             }
 
-          
             dbLayerDailyPlan.delete(filter);
             Globals.commit(sosHibernateSession);
 
@@ -176,6 +186,32 @@ public class DailyPlanSubmissionsImpl extends JOCResourceImpl implements IDailyP
             return JOCDefaultResponse.responseStatusJSError(e, getJocError());
         } finally {
             Globals.disconnect(sosHibernateSession);
+        }
+    }
+
+    private Date utc2Timezone(Date d, String fromTimeZone) throws ParseException {
+        String timeZone;
+        if (fromTimeZone == null) {
+            fromTimeZone = "UTC";
+        }
+        if (settings == null) {
+            timeZone = "Europe/Berlin";
+        } else {
+            timeZone = settings.getTimeZone();
+        }
+        SimpleDateFormat sdfFromTimezone = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        sdfFromTimezone.setTimeZone(TimeZone.getTimeZone(fromTimeZone));
+        SimpleDateFormat sdfToTimezone = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        sdfToTimezone.setTimeZone(TimeZone.getTimeZone(timeZone));
+        return sdfFromTimezone.parse(sdfToTimezone.format(d));
+    }
+
+    private void setSettings() throws Exception {
+        if (Globals.configurationGlobals != null) {
+
+            GlobalSettingsReader reader = new GlobalSettingsReader();
+            AConfigurationSection section = Globals.configurationGlobals.getConfigurationSection(DefaultSections.dailyplan);
+            this.settings = reader.getSettings(section);
         }
     }
 }
