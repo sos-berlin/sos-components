522d3289fdec6b511bc8c45d22f1171652da3517
SP <santiago.petzold@sos-berlin.com>
2021-05-20 14:22:59 +0200

    ./import - wrong source for foldername - fixed

--- sos-webservices/sos-webservices-joc/src/main/java/com/sos/joc/publish/util/ImportUtils.java
+++ sos-webservices/sos-webservices-joc/src/main/java/com/sos/joc/publish/util/ImportUtils.java
@@ -74,16 +74,18 @@ public class ImportUtils {
     	// update existing configuration from archive
     	updateableItem.getConfigurationObject().setName(updateableItem.getNewName());
     	if (updateableItem.getTargetFolder() != null && !updateableItem.getTargetFolder().isEmpty()) {
-    		if (!updateableItem.getConfigurationDbItem().getPath().startsWith(updateableItem.getTargetFolder())) {
-        		Path folder = Paths.get(updateableItem.getTargetFolder() + updateableItem.getConfigurationDbItem().getFolder()); 
-        		updateableItem.getConfigurationObject().setPath(folder.resolve(updateableItem.getNewName()).toString().replace('\\', '/'));
-    		} else if (!updateableItem.getConfigurationObject().getPath().startsWith(updateableItem.getTargetFolder())) {
-        		Path path = Paths.get(updateableItem.getTargetFolder() + updateableItem.getConfigurationObject().getPath()); 
-        		updateableItem.getConfigurationObject().setPath(path.getParent().resolve(updateableItem.getNewName()).toString().replace('\\', '/'));
-    		} else {
-        		updateableItem.getConfigurationObject().setPath(Paths.get(updateableItem.getConfigurationObject().getPath()).getParent().resolve(updateableItem.getNewName())
-        				.toString().replace('\\', '/'));
-    		}
+    		Path folder = Paths.get(updateableItem.getTargetFolder() + updateableItem.getConfigurationObject().getPath()).getParent();
+    		updateableItem.getConfigurationObject().setPath(folder.resolve(updateableItem.getNewName()).toString().replace('\\', '/'));
+//    		if (!updateableItem.getConfigurationDbItem().getPath().startsWith(updateableItem.getTargetFolder())) {
+//        		Path folder = Paths.get(updateableItem.getTargetFolder() + updateableItem.getConfigurationDbItem().getFolder()); 
+//        		updateableItem.getConfigurationObject().setPath(folder.resolve(updateableItem.getNewName()).toString().replace('\\', '/'));
+//    		} else if (!updateableItem.getConfigurationObject().getPath().startsWith(updateableItem.getTargetFolder())) {
+//        		Path path = Paths.get(updateableItem.getTargetFolder() + updateableItem.getConfigurationObject().getPath()); 
+//        		updateableItem.getConfigurationObject().setPath(path.getParent().resolve(updateableItem.getNewName()).toString().replace('\\', '/'));
+//    		} else {
+//        		updateableItem.getConfigurationObject().setPath(Paths.get(updateableItem.getConfigurationObject().getPath()).getParent().resolve(updateableItem.getNewName())
+//        				.toString().replace('\\', '/'));
+//    		}
     	} else {
     		updateableItem.getConfigurationObject().setPath(Paths.get(updateableItem.getConfigurationObject().getPath()).getParent().resolve(updateableItem.getNewName())
     				.toString().replace('\\', '/'));
