bdf2d9388c78d161d863db52439b805830fc6b6e
SP <santiago.petzold@sos-berlin.com>
2021-06-03 08:35:25 +0200

    SSHProviderTest - executeCommand test added

--- sos-commons/sos-commons-vfs/src/test/java/com/sos/commons/vfs/ssh/SSHProviderTest.java
+++ sos-commons/sos-commons-vfs/src/test/java/com/sos/commons/vfs/ssh/SSHProviderTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.sos.commons.util.common.SOSCommandResult;
 import com.sos.commons.vfs.common.AProviderArguments.Protocol;
 import com.sos.commons.vfs.common.proxy.Proxy;
 import com.sos.commons.vfs.ssh.common.SSHProviderArguments.AuthMethod;
@@ -19,9 +20,9 @@ public class SSHProviderTest {
     private static final Logger LOGGER = LoggerFactory.getLogger(SSHProviderTest.class);
 
     private static final String SSH_HOST = "localhost";
-    private static final int SSH_PORT = 22;
     private static final String SSH_USER = "sos";
     private static final String SSH_PASSWORD = "sos";
+    private static final int SSH_PORT = 22;
     private static final Path SSH_AUTH_FILE = Paths.get("C://id_rsa.ppk");
 
     private static final Proxy PROXY = new Proxy(Type.SOCKS, "proxy_host", 1080, "proxy_user", "12345", 30);
@@ -153,4 +154,36 @@ public class SSHProviderTest {
         }
     }
 
+    @Ignore
+    @Test
+    public void testExecuteCommand() throws Exception {
+        SSHProviderTestArguments args = new SSHProviderTestArguments();
+        args.setProtocol(Protocol.SSH);// not necessary - default
+        args.setHost(SSH_HOST);
+        args.setPort(SSH_PORT);
+
+        args.setAuthMethod(AuthMethod.PASSWORD);
+        args.setUser(SSH_USER);
+        args.setPassword(SSH_PASSWORD);
+
+        SSHProvider p = new SSHProvider(args);
+        try {
+            p.connect();
+            LOGGER.info(p.getServerInfo().toString());
+            SOSCommandResult result = p.executeCommand("ls -la /var/opt/apache-archiva/data/repositories");
+            LOGGER.info("STDOUT:\n" + result.getStdOut());
+            LOGGER.info("STERR: " + result.getStdErr());
+            LOGGER.info("Exit Code: " + result.getExitCode());
+            SOSCommandResult result2 = p.executeCommand("df -h");
+            LOGGER.info("STDOUT:\n" + result2.getStdOut());
+            LOGGER.info("STERR: " + result2.getStdErr());
+            LOGGER.info("Exit Code: " + result2.getExitCode());
+        } catch (Throwable e) {
+            throw e;
+        } finally {
+            if (p != null) {
+                p.disconnect();
+            }
+        }
+    }
 }
