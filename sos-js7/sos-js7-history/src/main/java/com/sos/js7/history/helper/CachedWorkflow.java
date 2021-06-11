c8a01df30d9b46c602b1a4075c846c62a81db611
SOS-RE <robert.ehrlich@sos-berlin.com>
2021-06-11 15:54:34 +0200

    history, monitoring - workflow title

--- sos-js7/sos-js7-history/src/main/java/com/sos/js7/history/helper/CachedWorkflow.java
+++ sos-js7/sos-js7-history/src/main/java/com/sos/js7/history/helper/CachedWorkflow.java
@@ -7,15 +7,21 @@ import com.sos.inventory.model.job.JobCriticality;
 public class CachedWorkflow {
 
     private final String path;
+    private final String title;
     private final Map<String, CachedWorkflowJob> jobs;
 
-    public CachedWorkflow(final String path, final Map<String, CachedWorkflowJob> jobs) {
+    public CachedWorkflow(final String path, final String title, final Map<String, CachedWorkflowJob> jobs) {
         this.path = path;
+        this.title = title;
         this.jobs = jobs;
     }
 
     public String getPath() {
         return path;
+    }
+
+    public String getTitle() {
+        return title;
     }
 
     public CachedWorkflowJob getJob(String name) {
