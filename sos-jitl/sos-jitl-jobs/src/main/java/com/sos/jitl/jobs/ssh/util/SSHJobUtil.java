443a746116c76e7e10b1948a19a3b3b3171bbb47
SOS-RE <robert.ehrlich@sos-berlin.com>
2021-06-14 10:12:50 +0200

    ssh job - checkExitCode NPE fixed

--- sos-jitl/sos-jitl-jobs/src/main/java/com/sos/jitl/jobs/ssh/util/SSHJobUtil.java
+++ sos-jitl/sos-jitl-jobs/src/main/java/com/sos/jitl/jobs/ssh/util/SSHJobUtil.java
@@ -11,17 +11,17 @@ import com.sos.jitl.jobs.ssh.SSHJobArguments;
 import com.sos.jitl.jobs.ssh.exception.SOSJobSSHException;
 
 public class SSHJobUtil {
-	
-	private static final String AGENT_ENVVAR_PREFIX = "JS7_AGENT_";
-	private static final String YADE_ENVVAR_PREFIX = "JS7_YADE_";
-	
+
+    private static final String AGENT_ENVVAR_PREFIX = "JS7_AGENT_";
+    private static final String YADE_ENVVAR_PREFIX = "JS7_YADE_";
+
     private static final String DEFAULT_WINDOWS_PRE_COMMAND = "set \"%s=%s\"";
     private static final String DEFAULT_LINUX_PRE_COMMAND = "export %s='%s'";
     public static final String DEFAULT_WINDOWS_POST_COMMAND_READ = "if exist \"%s\" type \"%s\"";
     public static final String DEFAULT_WINDOWS_POST_COMMAND_DELETE = "del \"%s\"";
     public static final String DEFAULT_LINUX_DELIMITER = ";";
     public static final String DEFAULT_WINDOWS_DELIMITER = "&";
-	public static final String JS7_RETURN_VALUES = "JS7_RETURN_VALUES";
+    public static final String JS7_RETURN_VALUES = "JS7_RETURN_VALUES";
 
     public static String resolve(SSHJobArguments jobArgs, String filename, boolean windowsShell) {
         if (jobArgs.getTmpDir().isDirty()) {
@@ -29,16 +29,16 @@ public class SSHJobUtil {
         } else {
             return filename;
         }
-	}
-	
-	public static void addPreCommand(SSHJobArguments jobArgs, StringBuilder sb, boolean windowsShell, String delimiter, String filepath) {
+    }
+
+    public static void addPreCommand(SSHJobArguments jobArgs, StringBuilder sb, boolean windowsShell, String delimiter, String filepath) {
         String preCommand = jobArgs.getPreCommand().getValue();
         if (!jobArgs.getPreCommand().isDirty()) {
             preCommand = windowsShell ? DEFAULT_WINDOWS_PRE_COMMAND : DEFAULT_LINUX_PRE_COMMAND;
         }
         sb.append(String.format(preCommand, JS7_RETURN_VALUES, filepath));
         sb.append(delimiter);
-	}
+    }
 
     private static String resolveTempFileName(String tempDir, String filename, boolean windowsShell) {
         if (windowsShell) {
@@ -50,8 +50,8 @@ public class SSHJobUtil {
 
     public static String substituteVariables(SOSParameterSubstitutor parameterSubstitutor, String source) {
         String result = source;
-        if(parameterSubstitutor == null) {
-        	parameterSubstitutor = createParameterSubstitutor();
+        if (parameterSubstitutor == null) {
+            parameterSubstitutor = createParameterSubstitutor();
         }
         if (source.matches("(?s).*\\$\\{[^{]+\\}.*")) {
             parameterSubstitutor.setOpenTag("${");
@@ -66,7 +66,7 @@ public class SSHJobUtil {
         return result;
     }
 
-    public static SOSParameterSubstitutor createParameterSubstitutor () {
+    public static SOSParameterSubstitutor createParameterSubstitutor() {
         SOSParameterSubstitutor parameterSubstitutor = new SOSParameterSubstitutor();
         for (Entry<String, String> entry : System.getenv().entrySet()) {
             String name = entry.getKey();
@@ -90,11 +90,13 @@ public class SSHJobUtil {
         }
     }
 
-    public static void checkExitCode(Integer exitCode, SSHJobArguments jobArgs, Map<String, Object> outcomes, JobLogger logger) throws SOSJobSSHException {
+    public static void checkExitCode(Integer exitCode, SSHJobArguments jobArgs, Map<String, Object> outcomes, JobLogger logger)
+            throws SOSJobSSHException {
         if (exitCode != null) {
             outcomes.put("exit_code", exitCode);
             if (!exitCode.equals(new Integer(0))) {
-                if (jobArgs.getIgnoreError().getValue() || jobArgs.getExitCodesToIgnore().getValue().contains(exitCode)) {
+                if (jobArgs.getIgnoreError().getValue() || (jobArgs.getExitCodesToIgnore().getValue() != null && jobArgs.getExitCodesToIgnore()
+                        .getValue().contains(exitCode))) {
                     outcomes.put("exit_code_ignored", true);
                     logger.info("SOS-SSH: exit code is ignored due to configuration: " + exitCode);
                 } else {
@@ -108,15 +110,15 @@ public class SSHJobUtil {
     }
 
     public static Map<String, String> getAgentEnvVars() {
-    	Map<String, String> unfiltered = System.getenv();
-    	return unfiltered.entrySet().stream().filter(item -> item.getKey().startsWith(AGENT_ENVVAR_PREFIX))
-    			.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        Map<String, String> unfiltered = System.getenv();
+        return unfiltered.entrySet().stream().filter(item -> item.getKey().startsWith(AGENT_ENVVAR_PREFIX)).collect(Collectors.toMap(
+                Map.Entry::getKey, Map.Entry::getValue));
     }
 
     public static Map<String, String> getYadeEnvVars() {
-    	Map<String, String> unfiltered = System.getenv();
-    	return unfiltered.entrySet().stream().filter(item -> item.getKey().startsWith(YADE_ENVVAR_PREFIX))
-    			.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        Map<String, String> unfiltered = System.getenv();
+        return unfiltered.entrySet().stream().filter(item -> item.getKey().startsWith(YADE_ENVVAR_PREFIX)).collect(Collectors.toMap(Map.Entry::getKey,
+                Map.Entry::getValue));
     }
 
 }
