8aaba532a69fb142f8f079ce667231a8862fc7c9
uwe-risse-sos <uwe.risse@sos-berlin.com>
2021-06-22 14:27:06 +0200

    SOSIniRealm

--- sos-webservices/sos-webservices-security/src/main/java/com/sos/auth/shiro/SOSPermissionResolverAdapter.java
+++ sos-webservices/sos-webservices-security/src/main/java/com/sos/auth/shiro/SOSPermissionResolverAdapter.java
@@ -6,15 +6,27 @@ import java.util.Collections;
 import org.apache.shiro.authz.Permission;
 import org.apache.shiro.authz.SimpleRole;
 import org.apache.shiro.authz.permission.RolePermissionResolver;
+import org.apache.shiro.config.IniSecurityManagerFactory;
 import org.apache.shiro.realm.text.IniRealm;
+
+import com.sos.joc.Globals;
 
 public class SOSPermissionResolverAdapter implements RolePermissionResolver {
 
     private LocalIniRealm realm;
 
+    @Override
     public Collection<Permission> resolvePermissionsInRole(final String roleString) {
         final SimpleRole role = this.realm.getRole(roleString);
-       
+
+        org.apache.shiro.config.IniSecurityManagerFactory factory = null;
+        String iniFile = Globals.getShiroIniInClassPath();
+        factory = new IniSecurityManagerFactory(Globals.getIniFileForShiro(iniFile));
+        factory.getIni();        
+        
+        this.realm.setIni(factory.getIni());
+        this.realm.init();
+
         if (role == null){
             return Collections.<Permission> emptySet();
         }else{
