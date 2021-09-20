4466097538222abeb5ef088670564fe2a15172be
Oliver Haufe <oliver.haufe.git@sos-berlin.com>
2021-09-20 17:51:06 +0200

    fix: restore creates subfolders too

--- sos-webservices/sos-webservices-json/src/main/java/com/sos/inventory/model/instruction/NamedJob.java
+++ sos-webservices/sos-webservices-json/src/main/java/com/sos/inventory/model/instruction/NamedJob.java
@@ -1,7 +1,6 @@
 
 package com.sos.inventory.model.instruction;
 
-import java.util.List;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyDescription;
