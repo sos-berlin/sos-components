c67b678358fcbf522d85c60762f17a5e7627fa57
SOS-RE <robert.ehrlich@sos-berlin.com>
2021-10-14 18:49:08 +0200

    WebservicePaths - daily_plan

--- sos-webservices/sos-webservices-joc-global/src/main/java/com/sos/joc/classes/WebservicePaths.java
+++ sos-webservices/sos-webservices-joc-global/src/main/java/com/sos/joc/classes/WebservicePaths.java
@@ -6,6 +6,7 @@ public class WebservicePaths {
     public static final String ORDER = "order";
     public static final String TASKS = "tasks";
     public static final String MONITORING = "monitoring";
+    public static final String DAILYPLAN = "daily_plan";
 
     public static String getResourceImplPath(String mainPath, final String subPath) {
         return String.format("./%s/%s", mainPath, subPath);
