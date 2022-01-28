66c1ea1d9c5781ac39000ecd2a44c7fc78c5f77e
SP <santiago.petzold@sos-berlin.com>
2022-01-28 12:00:56 +0100

    unused commons-collections dependency removed from detached sos-webservices-server project

--- sos-webservices/sos-webservices-server/src/test/java/com/sos/js7/joc/poc/jetty/TestEmbeddedJetty.java
+++ sos-webservices/sos-webservices-server/src/test/java/com/sos/js7/joc/poc/jetty/TestEmbeddedJetty.java
@@ -7,23 +7,18 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.security.KeyStore;
-import java.security.Principal;
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
 
 import javax.net.ssl.SSLContext;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpRequestRetryHandler;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.ssl.SSLContexts;
-import org.apache.http.ssl.TrustStrategy;
 import org.apache.http.util.EntityUtils;
 import org.junit.After;
 import org.junit.Before;
@@ -34,7 +29,6 @@ import org.junit.runners.MethodSorters;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.sos.joc2.poc.jetty.server.JettyServer;
 import com.sos.js7.joc.poc.jetty.server.JettyTestServer;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
