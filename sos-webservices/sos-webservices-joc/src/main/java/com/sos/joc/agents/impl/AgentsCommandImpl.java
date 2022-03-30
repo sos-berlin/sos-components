238796d3e13264f61cfcce0bbf21a355a72161de
Oliver Haufe <oliver.haufe.git@sos-berlin.com>
2022-03-30 19:46:59 +0200

    agents revoke

---
+++ sos-webservices/sos-webservices-joc/src/main/java/com/sos/joc/agents/impl/AgentsCommandImpl.java
@@ -0,0 +1,131 @@
+package com.sos.joc.agents.impl;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.ws.rs.Path;
+
+import com.sos.commons.hibernate.SOSHibernateSession;
+import com.sos.joc.Globals;
+import com.sos.joc.agents.resource.IAgentsCommand;
+import com.sos.joc.classes.JOCDefaultResponse;
+import com.sos.joc.classes.JOCResourceImpl;
+import com.sos.joc.classes.ProblemHelper;
+import com.sos.joc.classes.proxy.Proxy;
+import com.sos.joc.db.inventory.DBItemInventoryAgentInstance;
+import com.sos.joc.db.inventory.instance.InventoryAgentInstancesDBLayer;
+import com.sos.joc.event.EventBus;
+import com.sos.joc.event.bean.agent.AgentInventoryEvent;
+import com.sos.joc.exceptions.JocBadRequestException;
+import com.sos.joc.exceptions.JocException;
+import com.sos.joc.model.agent.DeployAgents;
+import com.sos.joc.model.audit.CategoryType;
+import com.sos.schema.JsonValidator;
+
+import io.vavr.control.Either;
+import js7.data.agent.AgentPath;
+import js7.data.subagent.SubagentId;
+import js7.data_for_java.agent.JAgentRef;
+import js7.data_for_java.controller.JControllerState;
+import js7.data_for_java.item.JUpdateItemOperation;
+import js7.proxy.javaapi.JControllerProxy;
+import reactor.core.publisher.Flux;
+
+@Path("agents")
+public class AgentsCommandImpl extends JOCResourceImpl implements IAgentsCommand {
+
+    private static final String API_CALL = "./agents/inventory/revoke";
+
+    @Override
+    public JOCDefaultResponse postRevoke(String accessToken, byte[] filterBytes) {
+        SOSHibernateSession connection = null;
+        try {
+            initLogging(API_CALL, filterBytes, accessToken);
+            JsonValidator.validateFailFast(filterBytes, DeployAgents.class);
+            DeployAgents agentDeployParameter = Globals.objectMapper.readValue(filterBytes, DeployAgents.class);
+            boolean permission = getJocPermissions(accessToken).getAdministration().getControllers().getManage();
+            
+            JOCDefaultResponse jocDefaultResponse = initPermissions("", permission);
+            if (jocDefaultResponse != null) {
+                return jocDefaultResponse;
+            }
+
+            String controllerId = agentDeployParameter.getControllerId();
+            List<String> agentIds = agentDeployParameter.getAgentIds();
+
+            storeAuditLog(agentDeployParameter.getAuditLog(), controllerId, CategoryType.CONTROLLER);
+
+            connection = Globals.createSosHibernateStatelessConnection(API_CALL);
+            InventoryAgentInstancesDBLayer agentDBLayer = new InventoryAgentInstancesDBLayer(connection);
+            List<DBItemInventoryAgentInstance> dbAgents = agentDBLayer.getAgentsByControllerIdAndAgentIds(Collections.singleton(controllerId),
+                    agentIds, false, false);
+            
+            if (dbAgents != null) {
+                agentIds.removeAll(dbAgents.stream().map(DBItemInventoryAgentInstance::getAgentId).collect(Collectors.toList()));
+            }
+            if (agentIds.size() > 0) {
+                throw new JocBadRequestException(String.format("The Agents %s are not assigned to Controller '%s'", agentIds.toString(),
+                        controllerId));
+            }
+            
+            List<JUpdateItemOperation> agentRefs = new ArrayList<>();
+            List<String> updateAgentIds = new ArrayList<>();
+            
+            JControllerProxy proxy = Proxy.of(controllerId);
+            JControllerState currentState = proxy.currentState();
+
+            if (dbAgents != null) {
+                Map<AgentPath, JAgentRef> knownAgents = currentState.pathToAgentRef();
+                for (DBItemInventoryAgentInstance dbAgent : dbAgents) {
+                    JAgentRef agentRef = knownAgents.get(AgentPath.of(dbAgent.getAgentId()));
+                    if (agentRef != null && (!agentRef.director().isPresent() || agentRef.directors().isEmpty())) {
+                        agentRefs.add(JUpdateItemOperation.deleteSimple(AgentPath.of(dbAgent.getAgentId())));
+                    } else {
+                        agentRefs.add(JUpdateItemOperation.deleteSimple(AgentPath.of(dbAgent.getAgentId())));
+                        agentRefs.add(JUpdateItemOperation.deleteSimple(SubagentId.of(dbAgent.getAgentId())));
+                    }
+                    updateAgentIds.add(dbAgent.getAgentId());
+                }
+            }
+
+            if (!agentRefs.isEmpty()) {
+                proxy.api().updateItems(Flux.fromIterable(agentRefs)).thenAccept(e -> {
+                    ProblemHelper.postProblemEventIfExist(e, getAccessToken(), getJocError(), controllerId);
+                    if (e.isRight()) {
+                        SOSHibernateSession connection1 = null;
+                        try {
+                            connection1 = Globals.createSosHibernateStatelessConnection(API_CALL);
+                            connection1.setAutoCommit(false);
+                            Globals.beginTransaction(connection1);
+                            InventoryAgentInstancesDBLayer dbLayer1 = new InventoryAgentInstancesDBLayer(connection1);
+                            dbLayer1.setAgentsDeployed(updateAgentIds, false);
+                            Globals.commit(connection1);
+                            EventBus.getInstance().post(new AgentInventoryEvent(controllerId, updateAgentIds));
+                        } catch (Exception e1) {
+                            Globals.rollback(connection1);
+                            ProblemHelper.postExceptionEventIfExist(Either.left(e1), accessToken, getJocError(), controllerId);
+                        } finally {
+                            Globals.disconnect(connection1);
+                        }
+                    }
+                });
+            }
+
+            return JOCDefaultResponse.responseStatusJSOk(Date.from(Instant.now()));
+        } catch (JocException e) {
+            Globals.rollback(connection);
+            e.addErrorMetaInfo(getJocError());
+            return JOCDefaultResponse.responseStatusJSError(e);
+        } catch (Exception e) {
+            Globals.rollback(connection);
+            return JOCDefaultResponse.responseStatusJSError(e, getJocError());
+        } finally {
+            Globals.disconnect(connection);
+        }
+    }
+}
---
+++ sos-webservices/sos-webservices-joc/src/main/java/com/sos/joc/agents/resource/IAgentsCommand.java
@@ -0,0 +1,18 @@
+package com.sos.joc.agents.resource;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import com.sos.joc.classes.JOCDefaultResponse;
+
+public interface IAgentsCommand {
+
+    @POST
+    @Path("inventory/deploy")
+    @Consumes("application/json")
+    @Produces({ "application/json" })
+    public JOCDefaultResponse postRevoke(@HeaderParam("X-Access-Token") String xAccessToken, byte[] filterBytes);
+}
