94012f00e2de867d4fe0d30ed9cfc4739f0f8959
uwe-risse-sos <uwe.risse@sos-berlin.com>
2022-04-08 11:33:28 +0200

    JOC-1230

--- sos-webservices/sos-webservices-joc-global/src/main/java/com/sos/auth/sosintern/SOSInternAuthHandler.java
+++ sos-webservices/sos-webservices-joc-global/src/main/java/com/sos/auth/sosintern/SOSInternAuthHandler.java
@@ -1,5 +1,7 @@
 package com.sos.auth.sosintern;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 import java.util.UUID;
 
 import org.apache.shiro.authc.credential.DefaultPasswordService;
@@ -9,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.sos.auth.classes.SOSAuthAccessToken;
+import com.sos.auth.classes.SOSAuthHelper;
 import com.sos.auth.classes.SOSPasswordHasher;
 import com.sos.auth.sosintern.classes.SOSInternAuthLogin;
 import com.sos.auth.sosintern.classes.SOSInternAuthWebserviceCredentials;
@@ -21,69 +24,79 @@ import com.sos.joc.db.security.IamAccountFilter;
 
 public class SOSInternAuthHandler {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(SOSInternAuthHandler.class);
-	private Boolean forcePasswordChange = false;
+    private static final Logger LOGGER = LoggerFactory.getLogger(SOSInternAuthHandler.class);
+    private Boolean forcePasswordChange = false;
 
-	public SOSInternAuthHandler() {
-	}
+    public SOSInternAuthHandler() {
+    }
 
-	private boolean isShiroMatch(String hash, String pwd) {
+    private boolean isShiroMatch(String hash, String pwd) {
 
-		String s = hash + "$$$$";
+        String s = hash + "$$$$";
 
-		String[] hashParts = s.split("\\$");
-		if ("shiro1".equals(hashParts[1])) {
+        String[] hashParts = s.split("\\$");
+        if ("shiro1".equals(hashParts[1])) {
 
-			PasswordMatcher p = new PasswordMatcher();
-			DefaultPasswordService ps = new DefaultPasswordService();
+            PasswordMatcher p = new PasswordMatcher();
+            DefaultPasswordService ps = new DefaultPasswordService();
 
-			DefaultHashService defaultHashService = new DefaultHashService();
-			defaultHashService.setHashAlgorithmName(hashParts[2]);
-			try {
-				defaultHashService.setHashIterations(Integer.valueOf(hashParts[3]));
-			} catch (NumberFormatException e) {
-				defaultHashService.setHashIterations(1);
-			}
-			defaultHashService.setGeneratePublicSalt(true);
-			ps.setHashService(defaultHashService);
-			ps.setHashFormat(new org.apache.shiro.crypto.hash.format.Shiro1CryptFormat());
-			p.setPasswordService(ps);
-			return p.getPasswordService().passwordsMatch(pwd, hash);
-		} else {
-			return false;
-		}
-	}
+            DefaultHashService defaultHashService = new DefaultHashService();
+            defaultHashService.setHashAlgorithmName(hashParts[2]);
+            try {
+                defaultHashService.setHashIterations(Integer.valueOf(hashParts[3]));
+            } catch (NumberFormatException e) {
+                defaultHashService.setHashIterations(1);
+            }
+            defaultHashService.setGeneratePublicSalt(true);
+            ps.setHashService(defaultHashService);
+            ps.setHashFormat(new org.apache.shiro.crypto.hash.format.Shiro1CryptFormat());
+            p.setPasswordService(ps);
+            return p.getPasswordService().passwordsMatch(pwd, hash);
+        } else {
+            return false;
+        }
+    }
 
-	public SOSAuthAccessToken login(SOSInternAuthWebserviceCredentials sosInternAuthWebserviceCredentials,
-			String password) throws SOSHibernateException {
+    public SOSAuthAccessToken login(SOSInternAuthWebserviceCredentials sosInternAuthWebserviceCredentials, String password)
+            throws SOSHibernateException {
 
-		SOSHibernateSession sosHibernateSession = null;
-		try {
-			sosHibernateSession = Globals.createSosHibernateStatelessConnection(SOSInternAuthLogin.class.getName());
-			SOSAuthAccessToken sosAuthAccessToken = null;
-			IamAccountDBLayer iamAccountDBLayer = new IamAccountDBLayer(sosHibernateSession);
-			forcePasswordChange = false;
-			IamAccountFilter filter = new IamAccountFilter();
-			filter.setAccountName(sosInternAuthWebserviceCredentials.getAccount());
-			filter.setIdentityServiceId(sosInternAuthWebserviceCredentials.getIdentityServiceId());
+        SOSHibernateSession sosHibernateSession = null;
+        try {
+            sosHibernateSession = Globals.createSosHibernateStatelessConnection(SOSInternAuthLogin.class.getName());
+            SOSAuthAccessToken sosAuthAccessToken = null;
+            IamAccountDBLayer iamAccountDBLayer = new IamAccountDBLayer(sosHibernateSession);
+            forcePasswordChange = false;
+            IamAccountFilter filter = new IamAccountFilter();
+            filter.setAccountName(sosInternAuthWebserviceCredentials.getAccount());
+            filter.setIdentityServiceId(sosInternAuthWebserviceCredentials.getIdentityServiceId());
 
-			DBItemIamAccount dbItemIamAccount = iamAccountDBLayer.getIamAccountByName(filter);
+            DBItemIamAccount dbItemIamAccount = iamAccountDBLayer.getIamAccountByName(filter);
 
-			if (dbItemIamAccount != null
-					&& (SOSPasswordHasher.verify(password, dbItemIamAccount.getAccountPassword()))
-					&& !dbItemIamAccount.getDisabled()) {
-				sosAuthAccessToken = new SOSAuthAccessToken();
-				sosAuthAccessToken.setAccessToken(UUID.randomUUID().toString());
-				forcePasswordChange = dbItemIamAccount.getForcePasswordChange();
-			}
-			return sosAuthAccessToken;
-		} finally {
-			Globals.disconnect(sosHibernateSession);
-		}
-	}
+            if (dbItemIamAccount != null && (SOSPasswordHasher.verify(password, dbItemIamAccount.getAccountPassword())) && !dbItemIamAccount
+                    .getDisabled()) {
+                sosAuthAccessToken = new SOSAuthAccessToken();
+                sosAuthAccessToken.setAccessToken(UUID.randomUUID().toString());
+                forcePasswordChange = dbItemIamAccount.getForcePasswordChange();
+                if (dbItemIamAccount.getAccountPassword().startsWith("$shiro")) {
+                    try {
+                        dbItemIamAccount.setAccountPassword(SOSPasswordHasher.hash(password));
+                    } catch (InvalidKeySpecException | NoSuchAlgorithmException e) {
+                        LOGGER.error("", e);
+                    }
+                    sosHibernateSession.setAutoCommit(false);
+                    sosHibernateSession.beginTransaction();
+                    sosHibernateSession.update(dbItemIamAccount);
+                    sosHibernateSession.commit();
+                }
+            }
+            return sosAuthAccessToken;
+        } finally {
+            Globals.disconnect(sosHibernateSession);
+        }
+    }
 
-	public Boolean getForcePasswordChange() {
-		return forcePasswordChange;
-	}
+    public Boolean getForcePasswordChange() {
+        return forcePasswordChange;
+    }
 
 }
