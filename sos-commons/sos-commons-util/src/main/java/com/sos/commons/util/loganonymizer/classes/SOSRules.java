7c1fa3096143de315b4a05e6d463e5e33367bfa9
uwe-risse-sos <uwe.risse@sos-berlin.com>
2022-05-11 15:19:05 +0200

    JS-1989

--- sos-commons/sos-commons-util/src/main/java/com/sos/commons/util/loganonymizer/classes/SOSRules.java
+++ sos-commons/sos-commons-util/src/main/java/com/sos/commons/util/loganonymizer/classes/SOSRules.java
@@ -5,45 +5,15 @@ import java.util.List;
 
 public class SOSRules {
 
-    private List<Rule> rules=new ArrayList<Rule>();
-    private List<Rule> agent=new ArrayList<Rule>();
-    private List<Rule> controller=new ArrayList<Rule>();
-    private List<Rule> jocCockpit=new ArrayList<Rule>();
-    
-    public List<Rule> getAgent() {
-        return agent;
-    }
-    
-    public void setAgent(List<Rule> agent) {
-        this.agent = agent;
-    }
-    
-    public List<Rule> getController() {
-        return controller;
-    }
-    
-    public void setController(List<Rule> controller) {
-        this.controller = controller;
-    }
-    
-    public List<Rule> getJocCockpit() {
-        return jocCockpit;
-    }
-    
-    public void setJocCockpit(List<Rule> jocCockpit) {
-        this.jocCockpit = jocCockpit;
-    }
+    private List<Rule> rules = new ArrayList<Rule>();
+ 
 
-    
     public List<Rule> getRules() {
         return rules;
     }
 
-    
     public void setRules(List<Rule> rules) {
         this.rules = rules;
     }
-     
-
 
 }
