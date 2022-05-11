bba701909720574edd912d0c194159777e2fc348
Andreas Püschel <andreas.pueschel@sos-berlin.com>
2022-05-11 19:47:24 +0200

    default rules updated

--- sos-commons/sos-commons-util/src/main/java/com/sos/commons/util/loganonymizer/classes/DefaultRulesTable.java
+++ sos-commons/sos-commons-util/src/main/java/com/sos/commons/util/loganonymizer/classes/DefaultRulesTable.java
@@ -10,14 +10,75 @@ public abstract class DefaultRulesTable {
     }
 
     private void init() {
-        a("ip-address",
+
+        a( "url-component",             // any log              ://apmacwin:4244
+                "://(.*):(\\d{3,5})", 
+                "<host>:<port>" );
+
+        a( "ip-address",                // any log              92.168.2.1
                 "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])",
-                "<ip-address>");
-        a("User", "User:.(.*)", "<user-id>");
-        a("agent_id", "agent=(.*)", "agent=<agent_id>");
-        a("host1", "Connect(.*.:.......)", "Connect(<host:port>");
-        a("user_name", "user_name.*=.*$", "user_name            = <user>");
-        a("user_home", "user_home.*=.*$", "user_home            = <user_home>");
+                "<ip-address>" );
+
+        a( "host-install",              // Install*.log         host                 = apmacwin
+                "host[\\s]*=[\\s]*(.*)",
+                "<host>" );
+
+        a( "host-http-install",         // Install*.log         http_host            = localhost
+                "http_host[\\s]*=[\\s]*(.*)",
+                "<host>" );
+
+        a( "port-http-install",         // Install*.log         http_host            = localhost
+                "http_port[\\s]*=[\\s]*(.*)",
+                "<port>" );
+
+        a( "host-status-install",       // Install*.log         "hostname": "APMACWIN"
+                "\\\"hostname\\\"[\\s]*:[\\s]*\\\"(.*)\\\"",
+                "<host>" );
+
+        a( "user-name",                 // Install*.log         user_name            = ap
+                "user_name[\\s]*=[\\s](.*)$", 
+                "<user>" );
+        
+        a( "user-home",                 // Install*.log         user_home            = C:\Users\ap
+                "user_home[\\s]*=[\\s]*(.*)$", 
+                "<user-home>" );
+
+        a( "jetty-port",                // Install*.log         jetty_port            = 4446
+                "jetty_port[\\s]*=[\\s]*(.*)$", 
+                "<jetty-port>" );
+
+        a( "jetty-stop-port",           // Install*.log         jetty_port            = 4446
+                "jetty_stop_port[\\s]*=[\\s]*(.*)$", 
+                "<jetty-port>" );
+
+        a( "jetty-user",                // Install*.log         jetty_user            = ap
+                "jetty_user[\\s]*=[\\s](.*)$", 
+                "<jetty-user>" );
+        
+        a( "jetty-user-home",           // Install*.log         jetty_user_home       = C:\Users\ap
+                "jetty_user_home[\\s]*=[\\s]*(.*)$", 
+                "<jetty-user-home>" );
+
+        a( "db-user",                   // Install*.log         user                  = jobscheduler
+                "user[\\s]*=[\\s](.*)$", 
+                "<db-user>" );
+        
+        a( "connect-component",         // joc.log              Connect(apmacwin:4344   Connect apmacwin:4344
+                "Connect[\\s\\(]*(.*):(\\d{3,5})", 
+                "<host>:<port>" );
+
+        a( "user-request",              // joc.log              USER: gauss@example.com
+                "USER:[ ]*(.*)[\\s]*", 
+                "<user-account>" );
+        
+        a( "user-login",                // controller.log       User:jobscheduler
+                "User:(.*)[\\s]*",
+                "<user-id>" );
+        
+        a( "agent-id",                  // controller.log
+                "agent[\\s]*=[\\s]*(.*)", 
+                "<agent-id>" );
+        
     }
 
 }
