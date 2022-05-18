6b55de4f2bca0cccbe71ec60ee4331af29bb6d42
SP <santiago.petzold@sos-berlin.com>
2022-05-18 09:50:33 +0200

    JITL-690

--- sos-jitl/sos-jitl-jobs/src/main/java/com/sos/jitl/jobs/examples/JocApiJob.java
+++ sos-jitl/sos-jitl-jobs/src/main/java/com/sos/jitl/jobs/examples/JocApiJob.java
@@ -1,7 +1,7 @@
 package com.sos.jitl.jobs.examples;
 
 import com.sos.jitl.jobs.common.ABlockingInternalJob;
-import com.sos.jitl.jobs.common.JobApiExecutor;
+import com.sos.jitl.jobs.common.JOCApiJobExecutor;
 import com.sos.jitl.jobs.common.JobStep;
 
 import js7.data_for_java.order.JOutcome;
@@ -15,13 +15,18 @@ public class JocApiJob extends ABlockingInternalJob<JocApiJobArguments> {
     @Override
     public JOutcome.Completed onOrderProcess(JobStep<JocApiJobArguments> step) throws Exception {
 //        JobApiExecutor ex = new JobApiExecutor(step.getArguments().getJocUri().getValue(), step.getArguments().getTrustoreFileName());
-        JobApiExecutor ex = new JobApiExecutor(step.getLogger());
+        JOCApiJobExecutor ex = new JOCApiJobExecutor(step.getLogger());
         try {
             String token = ex.login();
 
             step.getLogger().info("Logged in!");
             step.getLogger().info("accessToken: " + token);
-
+            
+            String relativeApiUrl = "/joc/api/orders/history";
+            String body = "{\"controllerId\":\"standalone\",\"dateFrom\":\"0d\",\"dateTo\":\"0d\",\"limit\":5000,\"timeZone\":\"Europe/Berlin\"}";
+            String response = ex.post(token, relativeApiUrl, body);
+            step.getLogger().info(response);
+            
             ex.logout(token);
             step.getLogger().info("Logged out!");
         } catch (Throwable e) {
