04084f9155b76641b99ee769daad3519712f2bf7
SOS-RE <robert.ehrlich@sos-berlin.com>
2022-05-30 12:48:28 +0200

    JOC-1154 - main (log4j dependency removed)

--- sos-js7/sos-js7-converter/sos-js7-converter-commons/src/main/java/com/sos/js7/converter/commons/JS7ConverterMain.java
+++ sos-js7/sos-js7-converter/sos-js7-converter-commons/src/main/java/com/sos/js7/converter/commons/JS7ConverterMain.java
@@ -1,13 +1,9 @@
 package com.sos.js7.converter.commons;
 
-import java.io.FileInputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import org.apache.logging.log4j.core.LoggerContext;
-import org.apache.logging.log4j.core.config.ConfigurationSource;
-import org.apache.logging.log4j.core.config.xml.XmlConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,16 +23,14 @@ public abstract class JS7ConverterMain {
     public abstract void doConvert(Path input, Path outputDir, Path reportDir) throws Exception;
 
     public void doMain(JS7ConverterConfig config, String[] args) {
-        if (args.length < 5) {
+        if (args.length < 4) {
             System.out.println("usage:");
             System.out.println("    <input dir>         - required - directory with XML files");
             System.out.println("    <output dir>        - required - output directory with the JS7 files");
             System.out.println("    <report dir>        - required - report directory");
             System.out.println("    <properties file>   - required - converter configuration properties file");
-            System.out.println("    <log4j2>            - required - log4j2 configuration file");
             return;
         }
-        LoggerContext lc = LoggerContext.getContext(false);
         int status = 0;
         try {
             Path input = Paths.get(args[0]);
@@ -46,8 +40,6 @@ public abstract class JS7ConverterMain {
             Path outputDir = Paths.get(args[1]);
             Path reportDir = Paths.get(args[2]);
             Path configFile = Paths.get(args[3].trim());
-
-            lc.start(new XmlConfiguration(lc, new ConfigurationSource(new FileInputStream(args[4].trim()))));
 
             System.out.println("------------------------");
             System.out.println("Input   = " + input.toAbsolutePath());
@@ -61,13 +53,6 @@ public abstract class JS7ConverterMain {
         } catch (Throwable e) {
             LOGGER.error(e.toString(), e);
             status = 1;
-        } finally {
-            if (lc != null) {
-                try {
-                    lc.stop();
-                } catch (Throwable e) {
-                }
-            }
         }
         System.exit(status);
     }
