a07445f9105915629696339149458534cad2a37d
SP <santiago.petzold@sos-berlin.com>
2022-06-16 13:51:57 +0200

    JOC-1323

---
+++ sos-webservices/sos-webservices-joc/src/test/java/com/sos/joc/publish/ImportTest.java
@@ -0,0 +1,19 @@
+package com.sos.joc.publish;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.EnumSet;
+
+import org.junit.Test;
+
+import com.sos.joc.model.inventory.common.ConfigurationType;
+import com.sos.joc.publish.util.ImportUtils;
+
+public class ImportTest {
+
+    @Test
+    public void testImportOrderLength() {
+        // -3 : because JOB, JOBCLASS and FOLDER are not im-/export objects
+        assertTrue(ImportUtils.getImportOrder().size() == EnumSet.allOf(ConfigurationType.class).size() -3);
+    }
+}
