c416d881018febb5e0a074b1ff8c0e26894332e0
oh <oliver.haufe@sos-berlin.com>
2022-09-09 13:04:24 +0200

    JOC-1367

--- sos-webservices/sos-webservices-server/src/main/java/com/sos/joc2/Main.java
+++ sos-webservices/sos-webservices-server/src/main/java/com/sos/joc2/Main.java
@@ -2,18 +2,16 @@ package com.sos.joc2;
 
 import java.io.IOException;
 import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.TimeZone;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.glassfish.grizzly.http.server.HttpServer;
-import org.glassfish.grizzly.servlet.WebappContext;
-import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
-import org.glassfish.jersey.media.multipart.MultiPartFeature;
-import org.glassfish.jersey.server.ResourceConfig;
-
-import jakarta.servlet.ServletRegistration;
+import org.glassfish.jersey.grizzly2.servlet.GrizzlyWebContainerFactory;
+import org.glassfish.jersey.server.ServerProperties;
 
  
 public class Main {
@@ -24,21 +22,18 @@ public class Main {
     /**
      * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
      * @return Grizzly HTTP server.
+     * @throws IOException 
      */
-    public static HttpServer startServer() {
-        // create a resource config that scans for JAX-RS resources and providers
-        // in com.sos.webservices package
-        final ResourceConfig rc = new ResourceConfig().packages("com.sos.webservices", "com.sos.joc", "com.sos.auth").register(
-                MultiPartFeature.class);
+    public static HttpServer startServer() throws IOException {
+        
+        Map<String, String> initParams = new HashMap<>();
+        initParams.put(ServerProperties.PROVIDER_PACKAGES, "com.sos.joc, com.sos.webservices.order, com.sos.auth.classes");
+        initParams.put(ServerProperties.PROVIDER_CLASSNAMES, "org.glassfish.jersey.media.multipart.MultiPartFeature");
 
         // create and start a new instance of grizzly http server
         // exposing the Jersey application at BASE_URI
         
-        HttpServer httpServer = GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
-        WebappContext context = new WebappContext("WebappContext", "");
-        ServletRegistration registration = context.addServlet("ServletContainer", new JocServletContainer());
-        registration.addMapping("/*");
-        context.deploy(httpServer);
+        HttpServer httpServer = GrizzlyWebContainerFactory.create(URI.create(BASE_URI), JocServletContainer.class, initParams);
         return httpServer;
     }
 
