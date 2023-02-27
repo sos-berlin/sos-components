1b5159755aab19c87a980b45d41833a661081909
uwe-risse-sos <uwe.risse@sos-berlin.com>
2023-02-27 14:20:43 +0100

    Avoid NPE in ClientCertificateHandler

--- sos-webservices/sos-webservices-joc-global/src/main/java/com/sos/auth/client/ClientCertificateHandler.java
+++ sos-webservices/sos-webservices-joc-global/src/main/java/com/sos/auth/client/ClientCertificateHandler.java
@@ -85,42 +85,41 @@ public class ClientCertificateHandler {
         }
     }
     
-    private String createResponse200 (String cipherSuiteName, Integer keySize) {
+    private String createResponse200(String cipherSuiteName, Integer keySize) {
         StringBuilder jsonStrb = new StringBuilder();
         jsonStrb.append("{\n");
         jsonStrb.append(String.format("  \"IssuerDN\" : \"%1$s\",\n", clientCertificate.getIssuerDN().getName()));
         jsonStrb.append(String.format("  \"SubjectDN\" : \"%1$s\",\n", subjectDN));
-        /*        
-         *      KeyUsage ::= BIT STRING {
-         *          digitalSignature        (0),
-         *          nonRepudiation          (1),
-         *          keyEncipherment         (2),
-         *          dataEncipherment        (3),
-         *          keyAgreement            (4),
-         *          keyCertSign             (5),
-         *          cRLSign                 (6),
-         *          encipherOnly            (7),
-         *          decipherOnly            (8)
-         *      }
+        /*
+         * KeyUsage ::= BIT STRING { digitalSignature (0), nonRepudiation (1), keyEncipherment (2), dataEncipherment (3), keyAgreement (4), keyCertSign (5),
+         * cRLSign (6), encipherOnly (7), decipherOnly (8) }
          */
         boolean[] keyUsages = clientCertificate.getKeyUsage();
-        jsonStrb.append("  \"keyUsages\" : [\n");
-        jsonStrb.append(String.format("      {\"digitalSignature\" : \"%1$s\"},\n", keyUsages[0]));
-        jsonStrb.append(String.format("      {\"nonRepudiation\" : \"%1$s\"},\n", keyUsages[1]));
-        jsonStrb.append(String.format("      {\"keyEncipherment\" : \"%1$s\"},\n", keyUsages[2]));
-        jsonStrb.append(String.format("      {\"dataEncipherment\" : \"%1$s\"},\n", keyUsages[3]));
-        jsonStrb.append(String.format("      {\"keyAgreement\" : \"%1$s\"},\n", keyUsages[4]));
-        jsonStrb.append(String.format("      {\"keyCertSign\" : \"%1$s\"},\n", keyUsages[5]));
-        jsonStrb.append(String.format("      {\"cRLSign\" : \"%1$s\"},\n", keyUsages[6]));
-        jsonStrb.append(String.format("      {\"encipherOnly\" : \"%1$s\"},\n", keyUsages[7]));
-        jsonStrb.append(String.format("      {\"decipherOnly\" : \"%1$s\"}\n", keyUsages[8]));
+        if (keyUsages != null) {
+            jsonStrb.append("  \"keyUsages\" : [\n");
+            jsonStrb.append(String.format("      {\"digitalSignature\" : \"%1$s\"},\n", keyUsages[0]));
+            jsonStrb.append(String.format("      {\"nonRepudiation\" : \"%1$s\"},\n", keyUsages[1]));
+            jsonStrb.append(String.format("      {\"keyEncipherment\" : \"%1$s\"},\n", keyUsages[2]));
+            jsonStrb.append(String.format("      {\"dataEncipherment\" : \"%1$s\"},\n", keyUsages[3]));
+            jsonStrb.append(String.format("      {\"keyAgreement\" : \"%1$s\"},\n", keyUsages[4]));
+            jsonStrb.append(String.format("      {\"keyCertSign\" : \"%1$s\"},\n", keyUsages[5]));
+            jsonStrb.append(String.format("      {\"cRLSign\" : \"%1$s\"},\n", keyUsages[6]));
+            jsonStrb.append(String.format("      {\"encipherOnly\" : \"%1$s\"},\n", keyUsages[7]));
+            jsonStrb.append(String.format("      {\"decipherOnly\" : \"%1$s\"}\n", keyUsages[8]));
+        }
         jsonStrb.append("  ],\n");
-        jsonStrb.append(String.format("  \"cipherSuiteName\" : \"%1$s\",\n", cipherSuiteName));
-        jsonStrb.append(String.format("  \"keySize\" : \"%1$d\",\n", keySize));
+        if (cipherSuiteName != null) {
+            jsonStrb.append(String.format("  \"cipherSuiteName\" : \"%1$s\",\n", cipherSuiteName));
+        }
+        if (keySize != null) {
+            jsonStrb.append(String.format("  \"keySize\" : \"%1$d\",\n", keySize));
+        }
         if (sslSessionIdHex != null) {
             jsonStrb.append(String.format("  \"SSLSessionId\" : \"%1$s\"\n", sslSessionIdHex));
         }
-        jsonStrb.append(String.format("  \"CN\" : \"%1$s\"\n", clientCN));
+        if (clientCN != null) {
+            jsonStrb.append(String.format("  \"CN\" : \"%1$s\"\n", clientCN));
+        }
         jsonStrb.append("}");
         return jsonStrb.toString();
     }
