8b3194a2f1bee0d3dbd22bc6dc3e4a14ce490276
oh <oliver.haufe@sos-berlin.com>
2023-04-26 15:55:06 +0200

    new search API

---
+++ sos-webservices/sos-webservices-json/src/main/java/com/sos/joc/model/inventory/search/ResponseBaseSearchItem.java
@@ -0,0 +1,151 @@
+
+package com.sos.joc.model.inventory.search;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.sos.joc.model.inventory.common.ConfigurationType;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+
+
+/**
+ * Inventory search response item
+ * <p>
+ * 
+ * 
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "path",
+    "name",
+    "objectType"
+})
+public class ResponseBaseSearchItem {
+
+    /**
+     * path
+     * <p>
+     * absolute path of an object.
+     * (Required)
+     * 
+     */
+    @JsonProperty("path")
+    @JsonPropertyDescription("absolute path of an object.")
+    private String path;
+    /**
+     * string without < and >
+     * <p>
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("name")
+    private String name;
+    /**
+     * configuration types
+     * <p>
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("objectType")
+    private ConfigurationType objectType;
+
+    /**
+     * path
+     * <p>
+     * absolute path of an object.
+     * (Required)
+     * 
+     */
+    @JsonProperty("path")
+    public String getPath() {
+        return path;
+    }
+
+    /**
+     * path
+     * <p>
+     * absolute path of an object.
+     * (Required)
+     * 
+     */
+    @JsonProperty("path")
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    /**
+     * string without < and >
+     * <p>
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("name")
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * string without < and >
+     * <p>
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("name")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * configuration types
+     * <p>
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("objectType")
+    public ConfigurationType getObjectType() {
+        return objectType;
+    }
+
+    /**
+     * configuration types
+     * <p>
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("objectType")
+    public void setObjectType(ConfigurationType objectType) {
+        this.objectType = objectType;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).append("path", path).append("name", name).append("objectType", objectType).toString();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(name).append(path).append(objectType).toHashCode();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if ((other instanceof ResponseBaseSearchItem) == false) {
+            return false;
+        }
+        ResponseBaseSearchItem rhs = ((ResponseBaseSearchItem) other);
+        return new EqualsBuilder().append(name, rhs.name).append(path, rhs.path).append(objectType, rhs.objectType).isEquals();
+    }
+
+}
---
+++ sos-webservices/sos-webservices-json/src/main/java/com/sos/joc/model/inventory/search/ResponseQuickSearch.java
@@ -0,0 +1,126 @@
+
+package com.sos.joc.model.inventory.search;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+
+
+/**
+ * Inventory search response
+ * <p>
+ * 
+ * 
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "deliveryDate",
+    "token",
+    "results"
+})
+public class ResponseQuickSearch {
+
+    /**
+     * timestamp
+     * <p>
+     * Value is UTC timestamp in ISO 8601 YYYY-MM-DDThh:mm:ss.sZ or empty
+     * (Required)
+     * 
+     */
+    @JsonProperty("deliveryDate")
+    @JsonPropertyDescription("Value is UTC timestamp in ISO 8601 YYYY-MM-DDThh:mm:ss.sZ or empty")
+    private Date deliveryDate;
+    /**
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("token")
+    private String token;
+    @JsonProperty("results")
+    private List<ResponseBaseSearchItem> results = new ArrayList<ResponseBaseSearchItem>();
+
+    /**
+     * timestamp
+     * <p>
+     * Value is UTC timestamp in ISO 8601 YYYY-MM-DDThh:mm:ss.sZ or empty
+     * (Required)
+     * 
+     */
+    @JsonProperty("deliveryDate")
+    public Date getDeliveryDate() {
+        return deliveryDate;
+    }
+
+    /**
+     * timestamp
+     * <p>
+     * Value is UTC timestamp in ISO 8601 YYYY-MM-DDThh:mm:ss.sZ or empty
+     * (Required)
+     * 
+     */
+    @JsonProperty("deliveryDate")
+    public void setDeliveryDate(Date deliveryDate) {
+        this.deliveryDate = deliveryDate;
+    }
+
+    /**
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("token")
+    public String getToken() {
+        return token;
+    }
+
+    /**
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("token")
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    @JsonProperty("results")
+    public List<ResponseBaseSearchItem> getResults() {
+        return results;
+    }
+
+    @JsonProperty("results")
+    public void setResults(List<ResponseBaseSearchItem> results) {
+        this.results = results;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).append("deliveryDate", deliveryDate).append("token", token).append("results", results).toString();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(deliveryDate).append(results).append(token).toHashCode();
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+        if ((other instanceof ResponseQuickSearch) == false) {
+            return false;
+        }
+        ResponseQuickSearch rhs = ((ResponseQuickSearch) other);
+        return new EqualsBuilder().append(deliveryDate, rhs.deliveryDate).append(results, rhs.results).append(token, rhs.token).isEquals();
+    }
+
+}
