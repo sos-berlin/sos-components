a856e53795ddaa5e90d4c3cffb32c36dac1a145c
uwe-risse-sos <uwe.risse@sos-berlin.com>
2023-05-15 12:51:52 +0200

    JOC-1546

--- sos-webservices/sos-webservices-json/src/main/java/com/sos/joc/model/security/fido2/Fido2RegistrationFilter.java
+++ sos-webservices/sos-webservices-json/src/main/java/com/sos/joc/model/security/fido2/Fido2RegistrationFilter.java
@@ -20,7 +20,6 @@ import org.apache.commons.lang.builder.ToStringBuilder;
 @JsonPropertyOrder({
     "identityServiceName",
     "accountName",
-    "token",
     "auditLog"
 })
 public class Fido2RegistrationFilter {
@@ -44,14 +43,6 @@ public class Fido2RegistrationFilter {
     @JsonProperty("accountName")
     private String accountName;
     /**
-     * string without < and >
-     * <p>
-     * 
-     * 
-     */
-    @JsonProperty("token")
-    private String token;
-    /**
      * auditParams
      * <p>
      * 
@@ -72,13 +63,11 @@ public class Fido2RegistrationFilter {
      * @param identityServiceName
      * @param auditLog
      * @param accountName
-     * @param token
      */
-    public Fido2RegistrationFilter(String identityServiceName, String accountName, String token, AuditParams auditLog) {
+    public Fido2RegistrationFilter(String identityServiceName, String accountName, AuditParams auditLog) {
         super();
         this.identityServiceName = identityServiceName;
         this.accountName = accountName;
-        this.token = token;
         this.auditLog = auditLog;
     }
 
@@ -131,28 +120,6 @@ public class Fido2RegistrationFilter {
     }
 
     /**
-     * string without < and >
-     * <p>
-     * 
-     * 
-     */
-    @JsonProperty("token")
-    public String getToken() {
-        return token;
-    }
-
-    /**
-     * string without < and >
-     * <p>
-     * 
-     * 
-     */
-    @JsonProperty("token")
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    /**
      * auditParams
      * <p>
      * 
@@ -176,12 +143,12 @@ public class Fido2RegistrationFilter {
 
     @Override
     public String toString() {
-        return new ToStringBuilder(this).append("identityServiceName", identityServiceName).append("accountName", accountName).append("token", token).append("auditLog", auditLog).toString();
+        return new ToStringBuilder(this).append("identityServiceName", identityServiceName).append("accountName", accountName).append("auditLog", auditLog).toString();
     }
 
     @Override
     public int hashCode() {
-        return new HashCodeBuilder().append(identityServiceName).append(auditLog).append(accountName).append(token).toHashCode();
+        return new HashCodeBuilder().append(identityServiceName).append(auditLog).append(accountName).toHashCode();
     }
 
     @Override
@@ -193,7 +160,7 @@ public class Fido2RegistrationFilter {
             return false;
         }
         Fido2RegistrationFilter rhs = ((Fido2RegistrationFilter) other);
-        return new EqualsBuilder().append(identityServiceName, rhs.identityServiceName).append(auditLog, rhs.auditLog).append(accountName, rhs.accountName).append(token, rhs.token).isEquals();
+        return new EqualsBuilder().append(identityServiceName, rhs.identityServiceName).append(auditLog, rhs.auditLog).append(accountName, rhs.accountName).isEquals();
     }
 
 }
