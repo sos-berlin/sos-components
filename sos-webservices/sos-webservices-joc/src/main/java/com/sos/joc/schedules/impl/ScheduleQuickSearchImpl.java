467b4fbcb27f4a72d0f3254a6da473bbd37976f3
oh <oliver.haufe@sos-berlin.com>
2023-06-20 13:26:15 +0200

    quick search for schedules

---
+++ sos-webservices/sos-webservices-joc/src/main/java/com/sos/joc/schedules/impl/ScheduleQuickSearchImpl.java
@@ -0,0 +1,45 @@
+package com.sos.joc.schedules.impl;
+
+import java.util.Collections;
+
+import com.sos.joc.Globals;
+import com.sos.joc.classes.JOCDefaultResponse;
+import com.sos.joc.classes.JOCResourceImpl;
+import com.sos.joc.classes.quicksearch.QuickSearchStore;
+import com.sos.joc.exceptions.JocException;
+import com.sos.joc.inventory.resource.IQuickSearchResource;
+import com.sos.joc.model.inventory.search.RequestQuickSearchFilter;
+import com.sos.joc.model.inventory.search.RequestSearchReturnType;
+import com.sos.joc.model.inventory.search.ResponseQuickSearch;
+import com.sos.schema.JsonValidator;
+
+import jakarta.ws.rs.Path;
+
+@Path("schedules")
+public class ScheduleQuickSearchImpl extends JOCResourceImpl implements IQuickSearchResource {
+    
+    private static final String API_CALL = "./schedules/quick/search";
+
+    @Override
+    public JOCDefaultResponse postSearch(final String accessToken, final byte[] inBytes) {
+        try {
+            initLogging(API_CALL, inBytes, accessToken);
+            JsonValidator.validateFailFast(inBytes, RequestQuickSearchFilter.class);
+            RequestQuickSearchFilter in = Globals.objectMapper.readValue(inBytes, RequestQuickSearchFilter.class);
+
+            JOCDefaultResponse response = initPermissions(null, getJocPermissions(accessToken).getDailyPlan().getView());
+            if (response != null) {
+                return response;
+            }
+            
+            in.setReturnTypes(Collections.singletonList(RequestSearchReturnType.SCHEDULE));
+            ResponseQuickSearch answer = QuickSearchStore.getAnswer(in, accessToken, folderPermissions, false);
+            return JOCDefaultResponse.responseStatus200(Globals.objectMapper.writeValueAsBytes(answer));
+        } catch (JocException e) {
+            e.addErrorMetaInfo(getJocError());
+            return JOCDefaultResponse.responseStatusJSError(e);
+        } catch (Throwable e) {
+            return JOCDefaultResponse.responseStatusJSError(e, getJocError());
+        }
+    }
+}
