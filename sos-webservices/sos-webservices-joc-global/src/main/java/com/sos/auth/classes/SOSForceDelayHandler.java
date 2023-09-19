ea4c1bbf6d2b7e4abb67047602cc30aaf2dc9703
uwe-risse-sos <uwe.risse@sos-berlin.com>
2023-09-19 13:39:32 +0200

    JOC-1615

--- sos-webservices/sos-webservices-joc-global/src/main/java/com/sos/auth/classes/SOSForceDelayHandler.java
+++ sos-webservices/sos-webservices-joc-global/src/main/java/com/sos/auth/classes/SOSForceDelayHandler.java
@@ -2,51 +2,103 @@ package com.sos.auth.classes;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.sos.joc.classes.JocWebserviceDataContainer;
-
 public class SOSForceDelayHandler {
 
-    private static final int FIRST_DELAY_LEVEL = 3;
+    private static final int FIRST_DELAY_LEVEL = 2;
     private static final int FORCED_LONG_DELAY = 30;
     private static final int FORCED_FIRST_DELAY = 2;
     private static final int MAX_FAILED_LOGINS = 20000;
     private static final Logger LOGGER = LoggerFactory.getLogger(SOSForceDelayHandler.class);
-    Map<String, Integer> failedLogins = new LinkedHashMap<String, Integer>();
+    Map<String, Integer> failedLoginsAccounts = new LinkedHashMap<String, Integer>();
+    Map<String, Integer> failedLoginsIp = new LinkedHashMap<String, Integer>();
 
-    public void addFailedLogin(SOSAuthCurrentAccount currentAccount) {
-        if (failedLogins.size() > MAX_FAILED_LOGINS) {
-            String firstFailedLoginKey = failedLogins.keySet().stream().findFirst().get();
-            failedLogins.remove(firstFailedLoginKey);
-        }
-
-        if (currentAccount.getAccountname() != null) {
-            Integer fails = failedLogins.get(currentAccount.getAccountname());
+    private Integer getFails(String key, Map<String, Integer> failedLogins) {
+        Integer fails = 1;
+        if (key != null) {
+            fails = failedLogins.get(key);
             if (fails != null) {
                 fails = fails + 1;
             } else {
                 fails = 1;
             }
-            failedLogins.put(currentAccount.getAccountname(), fails);
+        }
+        return fails;
+    }
+
+    private int getDelay(Integer fails) {
+        if (fails != null && fails > 0) {
+            if (fails <= FIRST_DELAY_LEVEL) {
+                return FORCED_FIRST_DELAY;
+            } else {
+                return FORCED_LONG_DELAY;
+            }
+        }
+        return 0;
+    }
+
+    private void addFailedLoginAccounts(SOSAuthCurrentAccount currentAccount) {
+        if (currentAccount.getAccountname() != null && !currentAccount.getAccountname().isEmpty()) {
+            if (failedLoginsAccounts.size() >= MAX_FAILED_LOGINS) {
+                String firstFailedLoginKey = failedLoginsAccounts.keySet().stream().findFirst().get();
+                failedLoginsAccounts.remove(firstFailedLoginKey);
+            }
+            failedLoginsAccounts.put(currentAccount.getAccountname(), getFails(currentAccount.getAccountname(), failedLoginsAccounts));
+        }
+    }
+
+    private void addFailedLoginIp(SOSAuthCurrentAccount currentAccount) {
+        if (currentAccount.getCallerIpAddress() != null && !currentAccount.getCallerIpAddress().isEmpty()) {
+            if (failedLoginsIp.size() >= MAX_FAILED_LOGINS) {
+                String firstFailedLoginKey = failedLoginsIp.keySet().stream().findFirst().get();
+                failedLoginsIp.remove(firstFailedLoginKey);
+            }
+            failedLoginsIp.put(currentAccount.getCallerIpAddress(), getFails(currentAccount.getCallerIpAddress(), failedLoginsIp));
+        }
+    }
+
+    private int forceDelayAccount(String accountName) {
+        if (accountName != null) {
+            Integer fails = failedLoginsAccounts.get(accountName);
+            return getDelay(fails);
+        }
+        return 0;
+    }
+
+    private int forceDelayIp(String ip) {
+        if (ip != null) {
+            Integer fails = failedLoginsIp.get(ip);
+            return getDelay(fails);
+        }
+        return 0;
+    }
+
+    public void addFailedLogin(SOSAuthCurrentAccount currentAccount) {
+        if (currentAccount.getAccountname() != null && !currentAccount.getAccountname().isEmpty()) {
+            addFailedLoginAccounts(currentAccount);
+            addFailedLoginIp(currentAccount);
         }
     }
 
     public void forceDelay(SOSAuthCurrentAccount currentAccount) {
-        if (currentAccount.getAccountname() != null) {
-            Integer fails = failedLogins.get(currentAccount.getAccountname());
+        int delay = 0;
+        delay = forceDelayAccount(currentAccount.getAccountname());
+        delay = delay + forceDelayIp(currentAccount.getCallerIpAddress());
+        if (delay > FORCED_LONG_DELAY) {
+            delay = FORCED_LONG_DELAY;
+        } else {
+            if (delay > FIRST_DELAY_LEVEL) {
+                delay = FIRST_DELAY_LEVEL;
+            }
+        }
+        if (delay > 0) {
             try {
-                if (fails != null && fails > 0) {
-                    if (fails < FIRST_DELAY_LEVEL) {
-                        TimeUnit.SECONDS.sleep(FORCED_FIRST_DELAY);
-                    } else {
-                        TimeUnit.SECONDS.sleep(FORCED_LONG_DELAY);
-                    }
-                }
+                LOGGER.debug("....force delay for " + delay + " seconds");
+                TimeUnit.SECONDS.sleep(delay);
             } catch (InterruptedException e) {
                 LOGGER.error("", e);
             }
@@ -54,10 +106,18 @@ public class SOSForceDelayHandler {
     }
 
     public void resetFailedLogin(SOSAuthCurrentAccount currentAccount) {
-        if (currentAccount.getAccountname() != null) {
-            Integer fails = failedLogins.get(currentAccount.getAccountname());
+        if (currentAccount.getAccountname() != null && !currentAccount.getAccountname().isEmpty()) {
+            Integer fails = failedLoginsAccounts.get(currentAccount.getAccountname());
             if (fails != null) {
-                failedLogins.remove(currentAccount.getAccountname());
+                LOGGER.debug("....Reset force delay for " + currentAccount.getAccountname());
+                failedLoginsAccounts.remove(currentAccount.getAccountname());
+            }
+        }
+        if (currentAccount.getCallerIpAddress() != null && !currentAccount.getCallerIpAddress().isEmpty()) {
+            Integer fails = failedLoginsIp.get(currentAccount.getCallerIpAddress());
+            if (fails != null) {
+                LOGGER.debug("....Reset force delay for " + currentAccount.getCallerIpAddress());
+                failedLoginsIp.remove(currentAccount.getCallerIpAddress());
             }
         }
     }
