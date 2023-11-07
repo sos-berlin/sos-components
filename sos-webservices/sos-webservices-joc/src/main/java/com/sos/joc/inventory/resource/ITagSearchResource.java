9a3628b2a80c41ab81af4cd659f1b5173b4337f4
oh <oliver.haufe@sos-berlin.com>
2023-11-07 17:31:04 +0100

    JOC-1637

---
+++ sos-webservices/sos-webservices-joc/src/main/java/com/sos/joc/inventory/resource/ITagSearchResource.java
@@ -0,0 +1,24 @@
+package com.sos.joc.inventory.resource;
+
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.HeaderParam;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import com.sos.joc.classes.JOCDefaultResponse;
+import com.sos.joc.classes.inventory.JocInventory;
+
+public interface ITagSearchResource {
+
+    public static final String PATH = "tag/search";
+    public static final String IMPL_PATH = JocInventory.getResourceImplPath(PATH);
+
+    @POST
+    @Path(PATH)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces({ MediaType.APPLICATION_JSON })
+    public JOCDefaultResponse postTagSearch(@HeaderParam("X-Access-Token") final String accessToken, final byte[] body);
+
+}
