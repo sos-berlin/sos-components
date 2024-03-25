37fafd134c90e0518fda28fd605e7750e68f2150
oh <oliver.haufe@sos-berlin.com>
2024-03-25 16:36:59 +0100

    aroundWriteTo logs URI in error case

--- sos-webservices/sos-webservices-joc-global/src/main/java/com/sos/joc/classes/GZIPContentEncodingCompress.java
+++ sos-webservices/sos-webservices-joc-global/src/main/java/com/sos/joc/classes/GZIPContentEncodingCompress.java
@@ -5,8 +5,10 @@ import java.io.OutputStream;
 import java.util.zip.GZIPOutputStream;
 
 import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.HttpHeaders;
 import jakarta.ws.rs.core.MultivaluedMap;
+import jakarta.ws.rs.core.UriInfo;
 import jakarta.ws.rs.ext.Provider;
 import jakarta.ws.rs.ext.WriterInterceptor;
 import jakarta.ws.rs.ext.WriterInterceptorContext;
@@ -17,18 +19,29 @@ import com.sos.joc.annotation.Compress;
 @Compress
 public class GZIPContentEncodingCompress implements WriterInterceptor {
 
+    @Context
+    private UriInfo uriInfo;
+    
     @Override
     public void aroundWriteTo(WriterInterceptorContext context) throws IOException, WebApplicationException {
 
-        MultivaluedMap<String, Object> headers = context.getHeaders();
-        headers.add(HttpHeaders.CONTENT_ENCODING, "gzip");
-        
-        if (!headers.containsKey("X-Uncompressed-Length")) {
-            final OutputStream outputStream = context.getOutputStream();
-            context.setOutputStream(new GZIPOutputStream(outputStream));
-        }
+        try {
+            MultivaluedMap<String, Object> headers = context.getHeaders();
+            headers.add(HttpHeaders.CONTENT_ENCODING, "gzip");
+            
+            if (!headers.containsKey("X-Uncompressed-Length")) {
+                final OutputStream outputStream = context.getOutputStream();
+                context.setOutputStream(new GZIPOutputStream(outputStream));
+            }
 
-        context.proceed();
+            context.proceed();
+        } catch (Exception e) {
+            if (uriInfo != null) {
+                System.out.println("-------------------Request-URI--------------------------------------------");
+                System.out.println(uriInfo.getRequestUri());
+            }
+            throw e;
+        }
     }
 
 }
