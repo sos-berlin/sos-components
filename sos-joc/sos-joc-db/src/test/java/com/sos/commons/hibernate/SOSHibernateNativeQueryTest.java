1f39d4f3c59268ed3d6fe6ce2e3263fa8882a177
SOS-RE <robert.ehrlich@sos-berlin.com>
2024-05-14 14:49:09 +0200

    JOC-1769

--- sos-joc/sos-joc-db/src/test/java/com/sos/commons/hibernate/SOSHibernateNativeQueryTest.java
+++ sos-joc/sos-joc-db/src/test/java/com/sos/commons/hibernate/SOSHibernateNativeQueryTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.sos.commons.util.SOSString;
 import com.sos.joc.db.DBLayer;
 
 public class SOSHibernateNativeQueryTest {
@@ -17,6 +18,59 @@ public class SOSHibernateNativeQueryTest {
     private static final Logger LOGGER = LoggerFactory.getLogger(SOSHibernateNativeQueryTest.class);
 
     /* Native SQL Queries */
+
+    @Ignore
+    @Test
+    public void testNativeSimpleList() throws Exception {
+        SOSHibernateFactory factory = null;
+        SOSHibernateSession session = null;
+        try {
+            factory = SOSHibernateTest.createFactory();
+            session = factory.openStatelessSession();
+
+            StringBuilder sql = new StringBuilder("select ");
+            sql.append(factory.quoteColumn("ho.ORDER_ID"));
+            sql.append(",").append(factory.quoteColumn("hos.ID"));
+            sql.append(",").append(factory.quoteColumn("hos.JOB_NAME"));
+            sql.append(",").append(factory.quoteColumn("hos.CREATED")).append(" ");
+            sql.append("from " + DBLayer.TABLE_HISTORY_ORDERS).append(" ho ");
+            sql.append(",").append(DBLayer.TABLE_HISTORY_ORDER_STEPS).append(" hos ");
+            sql.append("where ");
+            sql.append(factory.quoteColumn("ho.ID")).append("=").append(factory.quoteColumn("hos.HO_ID"));
+
+            NativeQuery<?> query = session.createNativeQuery(sql.toString());
+            query.setMaxResults(5); // only for this test
+
+            LOGGER.info("[getResultListAsMaps]-----------------------]");
+            int i = 0;
+            List<Map<String, Object>> result1 = session.getResultListAsMaps(query);
+            for (Map<String, Object> r : result1) {
+                i++;
+                LOGGER.info("-" + i + ")----------");
+                r.entrySet().stream().forEach(e -> {
+                    LOGGER.info("    [" + e.getKey() + "][" + (e.getValue() == null ? "" : e.getValue().getClass()) + "]" + e.getValue());
+                });
+            }
+
+            LOGGER.info("[getResultListAsStringMaps]-----------------------]");
+            i = 0;
+            List<Map<String, String>> result2 = session.getResultListAsStringMaps(query);
+            for (Map<String, String> r : result2) {
+                i++;
+                LOGGER.info("-" + i + ")----------");
+                r.entrySet().stream().forEach(e -> {
+                    LOGGER.info("    [" + e.getKey() + "][" + (e.getValue() == null ? "" : e.getValue().getClass()) + "]" + e.getValue());
+                });
+            }
+
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            if (factory != null) {
+                factory.close(session);
+            }
+        }
+    }
 
     @Ignore
     @Test
@@ -44,7 +98,7 @@ public class SOSHibernateNativeQueryTest {
             query.setMaxResults(10); // only for this test
             List<MyJoinEntity> result = session.getResultList(query);
             for (MyJoinEntity item : result) {
-                LOGGER.info(SOSHibernate.toString(item));
+                LOGGER.info(SOSString.toString(item));
             }
 
         } catch (Exception e) {
