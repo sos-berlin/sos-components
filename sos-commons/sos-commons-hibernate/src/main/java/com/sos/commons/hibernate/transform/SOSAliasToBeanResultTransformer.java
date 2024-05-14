83e7781371622b4ccfff967ef2677e172c81471d
SOS-RE <robert.ehrlich@sos-berlin.com>
2024-05-14 16:09:39 +0200

    JOC-1769

--- sos-commons/sos-commons-hibernate/src/main/java/com/sos/commons/hibernate/transform/SOSAliasToBeanResultTransformer.java
+++ sos-commons/sos-commons-hibernate/src/main/java/com/sos/commons/hibernate/transform/SOSAliasToBeanResultTransformer.java
@@ -1,7 +1,5 @@
 package com.sos.commons.hibernate.transform;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -26,30 +24,47 @@ public class SOSAliasToBeanResultTransformer<T> implements TupleTransformer<T> {
         if (tuple.length != aliases.length) {
             throw new IllegalArgumentException("Length of aliases array must match length of tuple array");
         }
-        try {
-            if (setter == null) {
-                initialize();
-            }
 
-            T obj = resultClass.getDeclaredConstructor().newInstance();
-            for (int i = 0; i < aliases.length; i++) {
-                invokeSetter(obj, aliases[i], tuple[i]);
-            }
-            return obj;
-        } catch (Throwable e) {
-            throw new RuntimeException("Failed to instantiate result class", e);
+        if (setter == null) {
+            initialize();
         }
+
+        T obj = newResultClassInstance();
+        for (int i = 0; i < aliases.length; i++) {
+            invokeSetter(obj, aliases[i], tuple[i]);
+        }
+        return obj;
     }
 
     private void initialize() {
-        setter = SOSReflection.getAllDeclaredMethodsAsMap(resultClass).entrySet().stream().filter(e -> e.getKey().startsWith("set") && e.getValue()
-                .getParameterCount() == 1).collect(Collectors.toMap(Entry::getKey, Entry::getValue));
+        try {
+            setter = SOSReflection.getAllDeclaredMethodsAsMap(resultClass).entrySet().stream().filter(e -> e.getKey().startsWith("set") && e
+                    .getValue().getParameterCount() == 1).collect(Collectors.toMap(Entry::getKey, Entry::getValue));
+        } catch (Throwable e) {
+            throw new RuntimeException("[" + resultClass + "]Failed to initialize setter methods", e);
+        }
     }
 
-    private void invokeSetter(Object obj, String fieldName, Object value) throws Exception {
-        Method m = setter.get(getSetterName(fieldName));
-        if (m != null) {
+    private T newResultClassInstance() {
+        T obj = null;
+        try {
+            obj = resultClass.getDeclaredConstructor().newInstance();
+        } catch (Throwable e) {
+            throw new RuntimeException("[" + resultClass + "]Failed to instantiate result class", e);
+        }
+        return obj;
+    }
+
+    private void invokeSetter(Object obj, String fieldName, Object value) {
+        String setterName = getSetterName(fieldName);
+        Method m = setter.get(setterName);
+        if (m == null) {
+            throw new RuntimeException("[" + resultClass + "][" + setterName + "]setter not found");
+        }
+        try {
             m.invoke(obj, value);
+        } catch (Throwable e) {
+            throw new RuntimeException("[" + resultClass + "][" + setterName + "]Failed to invoke setter method", e);
         }
     }
 
