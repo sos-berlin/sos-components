6323023cc021c7054620b4c9981ac7c97296d8cd
oh <oliver.haufe@sos-berlin.com>
2024-05-15 13:43:32 +0200

    JOC-1769

--- sos-webservices/sos-webservices-joc-global/src/main/java/com/sos/joc/db/inventory/items/InventoryTreeFolderItem.java
+++ sos-webservices/sos-webservices-joc-global/src/main/java/com/sos/joc/db/inventory/items/InventoryTreeFolderItem.java
@@ -6,16 +6,16 @@ import com.sos.joc.model.inventory.common.ResponseFolderItem;
 public class InventoryTreeFolderItem extends ResponseFolderItem {
 
     public void setCountDeployed(Long val) {
-        super.setHasDeployments(val.intValue() > 0);
+        setHasDeployments(val.intValue() > 0);
     }
     
     public void setCountReleased(Long val) {
-        super.setHasReleases(val.intValue() > 0);
+        setHasReleases(val.intValue() > 0);
     }
     
     public void setType(Integer val) {
         try {
-            super.setObjectType(ConfigurationType.fromValue(val));
+            setObjectType(ConfigurationType.fromValue(val));
         } catch (Throwable e) {// e.g. unknown type
         }
     }
