158aeefdc7930b0e24bcb34e355e40d3eb299616
oh <oliver.haufe@sos-berlin.com>
2024-05-17 12:10:02 +0200

    JOC-1609

--- sos-webservices/sos-webservices-joc-global/src/main/java/com/sos/joc/classes/reporting/CSVColumns.java
+++ sos-webservices/sos-webservices-joc-global/src/main/java/com/sos/joc/classes/reporting/CSVColumns.java
@@ -4,22 +4,22 @@ package com.sos.joc.classes.reporting;
 public enum CSVColumns {
 
     ID("id", "0"),
-    CONTROLLER_ID("controllerId", ""),
-    ORDER_ID("orderId", ""),
-    WORKFLOW_PATH("workflowPath", ""),
-    WORKFLOW_VERSION_ID("workflowVersionId", ""),
-    WORKFLOW_NAME("workflowName", ""),
-    POSITION("position", ""), //only order steps
-    JOB_NAME("jobName", ""), //only order steps
+    CONTROLLER_ID("controllerId", "''"),
+    ORDER_ID("orderId", "''"),
+    WORKFLOW_PATH("workflowPath", "''"),
+    WORKFLOW_VERSION_ID("workflowVersionId", "''"),
+    WORKFLOW_NAME("workflowName", "''"),
+    POSITION("position", "0"), //only order steps
+    JOB_NAME("jobName", "''"), //only order steps
     CRITICALITY("criticality", "0"), //only order steps
-    AGENT_ID("agentId", ""), //only order steps
-    AGENT_NAME("agentName", ""), //only order steps
-    START_TIME("startTime", ""),
-    PLANNED_TIME("startTimeScheduled", ""), //only orders
-    END_TIME("endTime", ""),
+    AGENT_ID("agentId", "''"), //only order steps
+    AGENT_NAME("agentName", "''"), //only order steps
+    START_TIME("startTime", "''"),
+    PLANNED_TIME("startTimeScheduled", "''"), //only orders
+    END_TIME("endTime", "''"),
     ERROR("error", "0"),
-    CREATED("created", ""),
-    MODIFIED("modified", ""),
+    CREATED("created", "''"),
+    MODIFIED("modified", "''"),
     ORDER_STATE("state", "99"), //only orders
     STATE("severity", "1");
     
@@ -41,7 +41,7 @@ public enum CSVColumns {
     }
     
     public String hqlValue() {
-        return "coalesce(" + dbColumn + ", '" + defaultValue + "')";
+        return "coalesce(" + dbColumn + ", " + defaultValue + ")";
     }
 
 }
