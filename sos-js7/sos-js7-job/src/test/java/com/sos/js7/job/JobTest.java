203318f82009d1ceff927bd3f1543cd2fdbb1d2d
SOS-RE <robert.ehrlich@sos-berlin.com>
2024-05-24 14:25:00 +0200

    JITL-740

--- sos-js7/sos-js7-job/src/test/java/com/sos/js7/job/JobTest.java
+++ sos-js7/sos-js7-job/src/test/java/com/sos/js7/job/JobTest.java
@@ -1,7 +1,5 @@
 package com.sos.js7.job;
 
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,13 +8,13 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.sos.commons.util.SOSString;
-import com.sos.commons.vfs.ssh.common.SSHProviderArguments;
-import com.sos.js7.job.exception.JobArgumentException;
+import com.sos.commons.encryption.EncryptionUtils;
 import com.sos.js7.job.helper.TestJob;
 import com.sos.js7.job.helper.TestJobArguments;
-import com.sos.js7.job.helper.TestJobSuperClass;
-import com.sos.js7.job.helper.TestJobWithoutJobArgumentsClass;
+import com.sos.js7.job.resolver.DefaultBase64ValueResolver;
+import com.sos.js7.job.resolver.DefaultEncryptionResolver;
+
+import js7.data_for_java.order.JOutcome;
 
 public class JobTest {
 
@@ -24,110 +22,30 @@ public class JobTest {
 
     @Ignore
     @Test
-    public void testArguments() {
-        Map<String, Object> map = new HashMap<>();
-        map.put("x", "y");
+    public void testBase64Resolver() throws Exception {
+        Map<String, Object> args = new HashMap<>();
+        args.put("test", DefaultBase64ValueResolver.getPrefix() + "xxx");
+        args.put("path", DefaultBase64ValueResolver.getPrefix() + "yyy");
 
-        for (Map.Entry<String, Object> e : map.entrySet()) {
-            LOGGER.info("1=" + e.getKey() + "=" + e.getValue());
-            JobArgument<?> arg = JobArgument.toExecuteJobArgument(e.getKey(), e.getValue());
-            LOGGER.info("2=" + e.getKey() + "=" + SOSString.toString(arg));
-        }
-
+        UnitTestJobHelper<TestJobArguments> h = new UnitTestJobHelper<>(new TestJob());
+        JOutcome.Completed result = h.processOrder(args);
+        LOGGER.info("###############################################");
+        LOGGER.info(String.format("[RESULT]%s", result));
     }
 
     @Ignore
     @Test
-    public void testABlockingJobWithoutArgumentClazzConstructor() throws Exception {
-        TestJobSuperClass job = new TestJobSuperClass();
+    public void testEncryptionResolver() throws Exception {
+        Map<String, Object> args = new HashMap<>();
+        args.put(DefaultEncryptionResolver.ARG_NAME_ENCIPHERMENT_CERTIFICATE, "xxx");
+        args.put(DefaultEncryptionResolver.ARG_NAME_ENCIPHERMENT_PRIVATE_KEY_PATH, "yyy");
 
-        LOGGER.info("job class=" + job.getClass());
-        LOGGER.info("job class(generic super class)=" + job.getClass().getGenericSuperclass());
-        LOGGER.info("job super class=" + job.getClass().getSuperclass());
-        LOGGER.info("job super class(generic super class)=" + job.getClass().getSuperclass().getGenericSuperclass());
+        args.put("test", DefaultEncryptionResolver.getPrefix() + "zzz");
 
-        TestJobArguments args = (TestJobArguments) getJobArgumensClass(job).getDeclaredConstructor().newInstance();
-        LOGGER.info("name(superClass)=" + args.getTestSuperClass().getName());
-        LOGGER.info("name=" + args.getTest().getName());
+        UnitTestJobHelper<TestJobArguments> h = new UnitTestJobHelper<>(new TestJob());
+        JOutcome.Completed result = h.processOrder(args);
+        LOGGER.info("###############################################");
+        LOGGER.info(String.format("[RESULT]%s", result));
     }
 
-    @Ignore
-    @Test
-    public void testExtendedABlockingJobWithoutArgumentClazzConstructor() throws Exception {
-        TestJob job = new TestJob();
-
-        LOGGER.info("job class=" + job.getClass());
-        LOGGER.info("job class(generic super class)=" + job.getClass().getGenericSuperclass());
-        LOGGER.info("job super class=" + job.getClass().getSuperclass());
-        LOGGER.info("job super class(generic super class)=" + job.getClass().getSuperclass().getGenericSuperclass());
-
-        TestJobArguments args = (TestJobArguments) getJobArgumensClass(job).getDeclaredConstructor().newInstance();
-        LOGGER.info("name(superClass)=" + args.getTestSuperClass().getName());
-        LOGGER.info("name=" + args.getTest().getName());
-    }
-
-    @Ignore
-    @Test
-    public void testEJobWithoutJobArgumentsClass() throws Exception {
-        TestJobWithoutJobArgumentsClass job = new TestJobWithoutJobArgumentsClass();
-
-        LOGGER.info("job class=" + job.getClass());
-        LOGGER.info("job class(generic super class)=" + job.getClass().getGenericSuperclass());
-        LOGGER.info("job super class=" + job.getClass().getSuperclass());
-        LOGGER.info("job super class(generic super class)=" + job.getClass().getSuperclass().getGenericSuperclass());
-
-        Object args = (Object) getJobArgumensClass(job).getDeclaredConstructor().newInstance();
-        LOGGER.info("args=" + args.getClass());
-    }
-
-    @Ignore
-    @Test
-    public void testApp2JobArguments() throws Exception {
-        SSHProviderArguments sftpArgs = new SSHProviderArguments();
-        JobArguments a = new JobArguments(sftpArgs);
-
-        if (a.getIncludedArguments() != null && a.getIncludedArguments().size() > 0) {
-            a.getIncludedArguments().entrySet().stream().forEach(e -> {
-                for (JobArgument<?> arg : e.getValue()) {
-                    // LOGGER.info(arg.getName());
-                    if (arg.getName().equals("protocol")) {
-                        LOGGER.info(SOSString.toString(arg));
-                    }
-                }
-            });
-        }
-
-    }
-
-    private static Class<?> getJobArgumensClass(Object instance) throws Exception {
-        Class<?> clazz = instance.getClass();
-        while (clazz.getSuperclass() != Job.class) {
-            clazz = clazz.getSuperclass();
-            if (clazz == null)
-                throw new JobArgumentException(String.format("super class not found for %s", instance.getClass()));
-        }
-        Type gsc = clazz.getGenericSuperclass();
-        try {
-            return (Class<?>) ((ParameterizedType) gsc).getActualTypeArguments()[0];
-        } catch (Throwable e) {
-            if (gsc.getTypeName().endsWith(">")) {
-                throw e;
-            }
-            return Object.class;
-        }
-    }
-
-    @Ignore
-    @Test
-    public void testTypes() {
-        Map<String, Object> m = new HashMap<>();
-        m.put("my_boolean", true);
-
-        Object o = m.get("my_boolean");
-        if (o instanceof Boolean) {
-            LOGGER.info("my_boolean= is Boolean");
-        }
-
-        LOGGER.info("my_boolean=" + o.getClass());
-    }
 }
