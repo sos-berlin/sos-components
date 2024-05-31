0993cdb143137dcd18a0c0bee08d9d3055ca2041
SP <santiago.petzold@sos-berlin.com>
2024-05-31 10:55:22 +0200

    JITL-740

--- sos-js7/sos-js7-job/src/main/java/com/sos/js7/job/resolver/DefaultEncryptionResolver.java
+++ sos-js7/sos-js7-job/src/main/java/com/sos/js7/job/resolver/DefaultEncryptionResolver.java
@@ -1,9 +1,19 @@
 package com.sos.js7.job.resolver;
 
+import java.nio.file.Paths;
+import java.security.PrivateKey;
+import java.security.cert.X509Certificate;
+import java.util.Base64;
 import java.util.List;
 import java.util.Map;
 
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
 import com.sos.commons.encryption.EncryptionUtils;
+import com.sos.commons.encryption.decrypt.Decrypt;
+import com.sos.commons.sign.keys.key.KeyUtil;
 import com.sos.commons.util.common.SOSArgumentHelper.DisplayMode;
 import com.sos.js7.job.JobArgument;
 import com.sos.js7.job.OrderProcessStepLogger;
@@ -12,6 +22,9 @@ public class DefaultEncryptionResolver implements IJobArgumentValueResolver {
 
     public static final String ARG_NAME_ENCIPHERMENT_CERTIFICATE = "encipherment_certificate";
     public static final String ARG_NAME_ENCIPHERMENT_PRIVATE_KEY_PATH = "encipherment_private_key_path";
+    
+    private static PrivateKey privKey;
+    private static X509Certificate cert;
 
     public static String getPrefix() {
         return EncryptionUtils.ENCRYPTION_IDENTIFIER;
@@ -19,17 +32,24 @@ public class DefaultEncryptionResolver implements IJobArgumentValueResolver {
 
     public static void resolve(List<JobArgument<?>> toResolve, OrderProcessStepLogger logger, Map<String, Object> allArguments) throws Exception {
 
-        String ec = getArgumentValue(allArguments, ARG_NAME_ENCIPHERMENT_CERTIFICATE);
-        String epkp = getArgumentValue(allArguments, ARG_NAME_ENCIPHERMENT_PRIVATE_KEY_PATH);
+        String enciphermentCertificate = getArgumentValue(allArguments, ARG_NAME_ENCIPHERMENT_CERTIFICATE);
+        String privateKeyPath = getArgumentValue(allArguments, ARG_NAME_ENCIPHERMENT_PRIVATE_KEY_PATH);
 
         // TODO: read PK
+        privKey = KeyUtil.getPrivateKey(Paths.get(privateKeyPath));
+        cert = KeyUtil.getX509Certificate(enciphermentCertificate);
+        
         // TODO: validate PK against certificate
         
         for (JobArgument<?> arg : toResolve) {
             try {
-                arg.applyValue(decrypt((String) arg.getValue(), epkp));
+                String decrypted = decrypt((String) arg.getValue(), privateKeyPath);
+                logger.info(decrypted);
+                arg.applyValue(decrypted);
+                System.out.println("decrypted: " + decrypted);
                 // TODO: activated before finalize
 //                arg.setDisplayMode(DisplayMode.MASKED);
+                arg.setDisplayMode(DisplayMode.UNMASKED);
             } catch (Throwable e) {
                 arg.setNotAcceptedValue(arg.getValue(), e);
             }
@@ -37,9 +57,17 @@ public class DefaultEncryptionResolver implements IJobArgumentValueResolver {
     }
 
     /*** TODO */
-    private static String decrypt(String valWithPrefix, String enciphermentPrivateKeyPath) {
+    private static String decrypt(String valWithPrefix, String enciphermentPrivateKeyPath) throws Exception {
         String val = valWithPrefix.substring(getPrefix().length());
-        return val;
+        String[] splittedValues = val.split(" ");
+        String encryptedKey = splittedValues[0];
+        String iv = splittedValues[1];
+        String encryptedValue = splittedValues[2];
+        SecretKey key = new SecretKeySpec(EncryptionUtils.decryptSymmetricKey(encryptedKey.getBytes(), privKey), "AES");
+        byte[] decodedIV = Base64.getDecoder().decode(iv);
+        String decryptedValue = Decrypt.decrypt(EncryptionUtils.CIPHER_ALGORITHM, encryptedValue, key,
+                new IvParameterSpec(decodedIV));
+        return decryptedValue;
     }
 
     private static String getArgumentValue(Map<String, Object> allArguments, String name) {
--- sos-js7/sos-js7-job/src/test/java/com/sos/js7/job/JobTest.java
+++ sos-js7/sos-js7-job/src/test/java/com/sos/js7/job/JobTest.java
@@ -1,5 +1,8 @@
 package com.sos.js7.job;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.cert.X509Certificate;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,7 +11,8 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.sos.commons.encryption.EncryptionUtils;
+import com.sos.commons.sign.keys.certificate.CertificateUtils;
+import com.sos.commons.sign.keys.key.KeyUtil;
 import com.sos.js7.job.helper.TestJob;
 import com.sos.js7.job.helper.TestJobArguments;
 import com.sos.js7.job.resolver.DefaultBase64ValueResolver;
@@ -33,14 +37,20 @@ public class JobTest {
         LOGGER.info(String.format("[RESULT]%s", result));
     }
 
-    @Ignore
+//    @Ignore
     @Test
     public void testEncryptionResolver() throws Exception {
+        Path certificatePath = Paths.get("/sp/tmp/myX509.crt"); 
+        X509Certificate cert = KeyUtil.getX509Certificate(certificatePath);
+        
         Map<String, Object> args = new HashMap<>();
-        args.put(DefaultEncryptionResolver.ARG_NAME_ENCIPHERMENT_CERTIFICATE, "xxx");
-        args.put(DefaultEncryptionResolver.ARG_NAME_ENCIPHERMENT_PRIVATE_KEY_PATH, "yyy");
+        args.put("test", DefaultBase64ValueResolver.getPrefix() + "xxx");
+        args.put("path", DefaultBase64ValueResolver.getPrefix() + "yyy");
+        args.put(DefaultEncryptionResolver.ARG_NAME_ENCIPHERMENT_CERTIFICATE, CertificateUtils.asPEMString(cert));
+        args.put(DefaultEncryptionResolver.ARG_NAME_ENCIPHERMENT_PRIVATE_KEY_PATH, "/sp/tmp/myECprivate.key");
 
-        args.put("test", DefaultEncryptionResolver.getPrefix() + "zzz");
+//        args.put("test", DefaultEncryptionResolver.getPrefix() + "zzz");
+        args.put("myPasswd", "enc://BPrshdAB5pj4TDmUS/h2gBj1GmHsqiCmzROhuRWvv9t84Cecr6EaKGgJIBNYaRfRwZOSld19ChO5Iuc8rNXkqm7EUpOEVsnRDN5NGjF1LG50A9Li87xWGRZW6PwsjKT4XOeYjUI= JL8/6uX2j5+nvk+SmELs5Q== 8g2unq5vs252FM/U6BGNHw==");
 
         UnitTestJobHelper<TestJobArguments> h = new UnitTestJobHelper<>(new TestJob());
         JOutcome.Completed result = h.processOrder(args);
