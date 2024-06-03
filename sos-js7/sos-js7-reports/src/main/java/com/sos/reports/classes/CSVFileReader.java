decbdd124363bc67cf710d408f413e8dbd859e37
Uwe Risse <uwe.risse@sos-berlin.com>
2024-06-03 17:55:40 +0200

    JOC-1609

--- sos-js7/sos-js7-reports/src/main/java/com/sos/reports/classes/CSVFileReader.java
+++ sos-js7/sos-js7-reports/src/main/java/com/sos/reports/classes/CSVFileReader.java
@@ -1,7 +1,6 @@
 package com.sos.reports.classes;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -16,7 +15,7 @@ public class CSVFileReader {
     private static final Logger LOGGER = LoggerFactory.getLogger(CSVFileReader.class);
     private static final String SEMICOLON_DELIMITER = ";";
 
-    public void readOrders(IReport report, ReportArguments reportArguments) {
+    public void readOrders(IReport report, ReportArguments reportArguments) throws IOException {
 
         LOGGER.debug("read data for report:" + report.getType() + "/" + reportArguments.reportId);
         Interval interval = new Interval();
@@ -28,58 +27,51 @@ public class CSVFileReader {
 
             if (Files.exists(path)) {
 
-                LOGGER.debug("File:" + path.getFileName());
-                try (BufferedReader br = new BufferedReader(new FileReader(path.toFile()))) {
-                    String line;
-                    try {
-                        line = br.readLine();
-                        while ((line = br.readLine()) != null) {
-                            String[] values = line.split(SEMICOLON_DELIMITER);
-                            ReportRecord orderRecord = new ReportRecord();
-                            orderRecord.setId(values[0]);
-                            orderRecord.setControllerId(values[1]);
-                            if (reportArguments.controllerId == null || reportArguments.controllerId.isEmpty() || reportArguments.controllerId.equals(
-                                    orderRecord.getControllerId())) {
-                                orderRecord.setOrderId(values[2]);
-                                orderRecord.setWorkflowPath(values[3]);
-                                orderRecord.setWorkflowVersionId(values[4]);
-                                orderRecord.setWorkflowName(values[5]);
-                                orderRecord.setStartTime(values[6]);
-                                orderRecord.setPlannedTime(values[7]);
-                                orderRecord.setEndTime(values[8]);
-                                orderRecord.setError(values[9]);
-                                orderRecord.setCreated(values[10]);
-                                orderRecord.setModified(values[11]);
-                                orderRecord.setOrderState(values[12]);
-                                orderRecord.setState(values[13]);
+                LOGGER.debug("File:" + path.getFileName() + " in " + path.getParent());
+                BufferedReader br = new BufferedReader(new FileReader(path.toFile()));
+                String line;
 
-                                if (reportArguments.reportFrequency.endOfInterval(orderRecord.getStartTime().toLocalDate())) {
-                                    LOGGER.debug("Interval end reached:" + reportArguments.reportFrequency.getFrom() + " to "
-                                            + reportArguments.reportFrequency.getTo());
+                line = br.readLine();
+                while ((line = br.readLine()) != null) {
+                    String[] values = line.split(SEMICOLON_DELIMITER);
+                    ReportRecord orderRecord = new ReportRecord();
+                    orderRecord.setId(values[0]);
+                    orderRecord.setControllerId(values[1]);
+                    if (reportArguments.controllerId == null || reportArguments.controllerId.isEmpty() || reportArguments.controllerId.equals(
+                            orderRecord.getControllerId())) {
+                        orderRecord.setOrderId(values[2]);
+                        orderRecord.setWorkflowPath(values[3]);
+                        orderRecord.setWorkflowVersionId(values[4]);
+                        orderRecord.setWorkflowName(values[5]);
+                        orderRecord.setStartTime(values[6]);
+                        orderRecord.setPlannedTime(values[7]);
+                        orderRecord.setEndTime(values[8]);
+                        orderRecord.setError(values[9]);
+                        orderRecord.setCreated(values[10]);
+                        orderRecord.setModified(values[11]);
+                        orderRecord.setOrderState(values[12]);
+                        orderRecord.setState(values[13]);
 
-                                    report.putHits();
-                                    report.reset();
-                                    reportArguments.reportFrequency.nextPeriod();
-                                    LOGGER.debug("new frequency interval:" + reportArguments.reportFrequency.getFrom() + " to "
-                                            + reportArguments.reportFrequency.getTo());
-                                }
-                                report.count(orderRecord);
-                            }
+                        if (reportArguments.reportFrequency.endOfInterval(orderRecord.getStartTime().toLocalDate())) {
+                            LOGGER.debug("Interval end reached:" + reportArguments.reportFrequency.getFrom() + " to "
+                                    + reportArguments.reportFrequency.getTo());
+
+                            report.putHits();
+                            report.reset();
+                            reportArguments.reportFrequency.nextPeriod();
+                            LOGGER.debug("new frequency interval:" + reportArguments.reportFrequency.getFrom() + " to "
+                                    + reportArguments.reportFrequency.getTo());
                         }
-                    } catch (IOException e) {
-                        e.printStackTrace();
+                        report.count(orderRecord);
                     }
-                } catch (FileNotFoundException e1) {
-                    e1.printStackTrace();
-                } catch (IOException e1) {
-                    e1.printStackTrace();
                 }
+                report.putHits();
             } else {
                 LOGGER.debug("File:" + path.getFileName() + " not found in " + path.getParent());
             }
             interval.next();
+            reportArguments.reportFrequency.nextPeriod();
         }
-        report.putHits();
     }
 
     public void readJobs(IReport report, ReportArguments reportArguments) throws IOException {
@@ -94,7 +86,7 @@ public class CSVFileReader {
 
             if (Files.exists(path)) {
 
-                LOGGER.debug("File:" + path.getFileName());
+                LOGGER.debug("File:" + path.getFileName() + " in " + path.getParent());
                 Files.newBufferedReader(path);
                 BufferedReader br = Files.newBufferedReader(path);
 
@@ -133,20 +125,19 @@ public class CSVFileReader {
 
                             report.putHits();
                             report.reset();
-                            reportArguments.reportFrequency.nextPeriod();
                             LOGGER.debug("new frequency interval:" + reportArguments.reportFrequency.getFrom() + " to "
                                     + reportArguments.reportFrequency.getTo());
                         }
                         report.count(jobRecord);
                     }
                 }
-
+                report.putHits();
             } else {
-                LOGGER.debug("File:" + path.getFileName() + " not found");
+                LOGGER.debug("File:" + path.getFileName() + " not found in " + path.getParent());
             }
             interval.next();
+            reportArguments.reportFrequency.nextPeriod();
         }
-        report.putHits();
     }
 
 }
--- sos-js7/sos-js7-reports/src/test/java/com/sos/reports/ReportGeneratorTestMain.java
+++ sos-js7/sos-js7-reports/src/test/java/com/sos/reports/ReportGeneratorTestMain.java
@@ -23,9 +23,9 @@ public class ReportGeneratorTestMain {
         String hits = "3";
         String inputDirectory = "c:/temp/in";
         String outputDirectory = "c:/temp";
-        String frequency = "every3years";
-        String monthFrom = "2023-09";
-        String monthTo = "2023-09";
+        String frequency = "monthly";
+        String monthFrom = "2023-01";
+        String monthTo = "2023-12";
         String controller = "controller";
         String sort = "HIGHEST";
         
