7462940812f55f1f997e67d2f1893b4e2c95f886
SOS-RE <robert.ehrlich@sos-berlin.com>
2024-07-10 10:16:32 +0200

    JOC-1769 - some UnitTest resources removed

--- sos-jitl/sos-jitl-jobs/src/test/java/com/sos/jitl/jobs/db/hibernateProxy/create_tables.sql
+++
@@ -1,8 +0,0 @@
-set SQL_MODE=ANSI_QUOTES;
-
-CREATE TABLE IF NOT EXISTS TEST_HIBERNATE_PROXY(
-    "ID"                BIGINT(20)      UNSIGNED    NOT NULL    AUTO_INCREMENT,
-    "NAME"             	VARCHAR(255)                NOT NULL,
-    "VALUE"             VARCHAR(255)                NOT NULL,
-    PRIMARY KEY ("ID")
-) ENGINE=InnoDB;
