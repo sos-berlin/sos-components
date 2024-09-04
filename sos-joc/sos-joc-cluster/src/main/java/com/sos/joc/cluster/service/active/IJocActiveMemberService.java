b809d043ee49178a3d4127d9d64551c61046e3d0
SOS-RE <robert.ehrlich@sos-berlin.com>
2024-09-04 17:01:32 +0200

    JOC-1731

--- sos-joc/sos-joc-cluster/src/main/java/com/sos/joc/cluster/service/active/IJocActiveMemberService.java
+++ sos-joc/sos-joc-cluster/src/main/java/com/sos/joc/cluster/service/active/IJocActiveMemberService.java
@@ -11,23 +11,30 @@ import com.sos.joc.cluster.configuration.globals.common.AConfigurationSection;
 
 public interface IJocActiveMemberService {
 
+    // service identifier
+    public String getIdentifier();
+
+    // service start
     public JocClusterAnswer start(StartupMode mode, List<ControllerConfiguration> controllers, AConfigurationSection configuration);
+
+    // service stop
+    public JocClusterAnswer stop(StartupMode mode);
+
+    // run service implementation immediately - when the service starts at a specific time (e.g. cleanup service)
+    public void runNow(StartupMode mode, AConfigurationSection configuration);
+
+    // react when controllers have changed (added, removed ...)
+    public void update(StartupMode mode, List<ControllerConfiguration> controllers, String controllerId, Action action);
+
+    // react when settings have changed
+    public void update(StartupMode mode, AConfigurationSection configuration);
+
+    // service information - is busy etc ...
+    public JocServiceAnswer getInfo();
 
     public String getControllerApiUser();
 
     public String getControllerApiUserPassword();
 
-    public String getIdentifier();
-
-    public JocClusterAnswer stop(StartupMode mode);
-
     public ThreadGroup getThreadGroup();
-
-    public JocServiceAnswer getInfo();
-
-    public void update(StartupMode mode, List<ControllerConfiguration> controllers, String controllerId, Action action);
-
-    public void update(StartupMode mode, AConfigurationSection configuration);
-
-    public void runNow(StartupMode mode, AConfigurationSection configuration);
 }
