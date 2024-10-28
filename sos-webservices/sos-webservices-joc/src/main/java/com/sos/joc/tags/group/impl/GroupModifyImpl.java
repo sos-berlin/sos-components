f2e8ab8a2db2c43995717931f1ef8b476a6ad0ab
oh <oliver.haufe@sos-berlin.com>
2024-10-28 13:45:15 +0100

    JOC-1811

--- sos-webservices/sos-webservices-joc/src/main/java/com/sos/joc/tags/group/impl/GroupModifyImpl.java
+++ sos-webservices/sos-webservices-joc/src/main/java/com/sos/joc/tags/group/impl/GroupModifyImpl.java
@@ -6,9 +6,7 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import com.sos.commons.hibernate.SOSHibernateSession;
