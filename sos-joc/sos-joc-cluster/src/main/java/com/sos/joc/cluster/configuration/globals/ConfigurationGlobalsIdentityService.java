701fe82daffc4f14bfc603a1081c2ad27cb91fbc
Uwe Risse <uwe.risse@sos-berlin.com>
2024-10-31 11:20:47 +0100

    JOC-1929

---
+++ sos-joc/sos-joc-cluster/src/main/java/com/sos/joc/cluster/configuration/globals/ConfigurationGlobalsIdentityService.java
@@ -0,0 +1,35 @@
+package com.sos.joc.cluster.configuration.globals;
+
+import com.sos.joc.cluster.configuration.globals.common.AConfigurationSection;
+import com.sos.joc.cluster.configuration.globals.common.ConfigurationEntry;
+import com.sos.joc.model.configuration.globals.GlobalSettingsSectionValueType;
+
+public class ConfigurationGlobalsIdentityService extends AConfigurationSection {
+
+    private static final String INITIAL = "initial";
+    private ConfigurationEntry idleSessionTimeout = new ConfigurationEntry("idle_session_timeout", "60s",
+            GlobalSettingsSectionValueType.DURATION);
+    private ConfigurationEntry initialPassword = new ConfigurationEntry("initial_password", INITIAL, GlobalSettingsSectionValueType.PASSWORD);
+    private ConfigurationEntry minimumPasswordLength = new ConfigurationEntry("minimum_password_length", "0",
+            GlobalSettingsSectionValueType.NONNEGATIVEINTEGER);
+
+    public ConfigurationGlobalsIdentityService() {
+        int index = -1;
+        idleSessionTimeout.setOrdering(++index);
+        initialPassword.setOrdering(++index);
+        minimumPasswordLength.setOrdering(++index);
+    }
+
+    public ConfigurationEntry getIdleSessionTimeout() {
+        return idleSessionTimeout;
+    }
+
+    public ConfigurationEntry getInitialPassword() {
+        return initialPassword;
+    }
+
+    public ConfigurationEntry getMininumPasswordLength() {
+        return minimumPasswordLength;
+    }
+
+}
