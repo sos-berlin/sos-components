0f15d43a11583007d39e76a83fc48c0400f8aa8a
SOS-RE <robert.ehrlich@sos-berlin.com>
2024-11-19 12:02:58 +0100

    JOC-1931 - use the "..-h2/target/generated-sources" directory to separate Executor and copied sources (additionally to avoid a git commit when placed in java/main)

--- setup-helper/setup-helper-h2/src/main/java/setup/helper/h2/Executor.java
+++ setup-helper/setup-helper-h2/src/main/java/setup/helper/h2/Executor.java
@@ -17,52 +17,53 @@ public class Executor {
     private static final String REPLACEMENT = "@ColumnTransformer(write = \"" + SOSHibernateJsonType.COLUMN_TRANSFORMER_WRITE_H2 + "\")";
 
     /** Arguments:<br/>
-     * 1) Path of the base directory of the current project<br/>
-     * -- ${project.basedir}<br/>
-     * 2) Relative path of the source project (including the root package)<br/>
-     * -- sos-joc/sos-joc-db/src/main/java/com<br/>
+     * 1) Relative path of the source project<br/>
+     * -- sos-joc/sos-joc-db/src/main/java<br/>
+     * 2) Path of the generated sources directory of the current project - target/generated-sources<br/>
+     * -- ${generated.sources.dir} (${project.build.directory}/generated-sources)<br/>
      * 
      * @param args
      * @throws Exception */
     public static void main(String[] args) throws Exception {
         if (args.length < 2) {
-            throw new Exception("[missing arguments]currentProject/sourceProject");
+            throw new Exception("[missing arguments]sourceProject/generatedSources");
         }
-        Path currentProject = Paths.get(args[0]).toAbsolutePath();
-        Path sourceProject = Paths.get(args[1]);// without toAbsolutePath
+        Path sourceProject = Paths.get(args[0]);// without toAbsolutePath
+        Path currentProjectGeneratedSources = Paths.get(args[1]).toAbsolutePath();
 
-        Path source = getSourceDir(currentProject, sourceProject);
-        Path target = getTargetDir(currentProject);
-        Files.createDirectories(target);
+        Path source = getSourceDir(currentProjectGeneratedSources, sourceProject);
+        Path target = currentProjectGeneratedSources;
+
+        // is not really necessary, since the target directory is deleted/recreated with Maven clean etc.
+        prepareTarget(target);
+
+        System.out.println("[source]" + source);
+        System.out.println("[target]" + target);
 
         Files.walk(source).filter(Files::isRegularFile).filter(path -> path.toString().endsWith(".java")).forEach(path -> processDBItemFile(source,
                 path, target));
 
     }
 
-    private static Path getSourceDir(Path currentProject, Path sourceProject) throws Exception {
-        // <sos-components> / setup-helper / setup-helper-h2
-        // build environment uses e.g. ../020.sos-components.v2.7.centos8/setup-helper/setup-helper-h2
-        Path mainProjectDir = currentProject.getParent();
-        if (mainProjectDir == null) {
-            throw new Exception("[currentProject=" + currentProject + "][1]parent is null");
+    private static Path getSourceDir(Path currentProjectGeneratedSources, Path sourceProject) throws Exception {
+        // <sos-components> / setup-helper / setup-helper-h2 / target / generated-sources
+        // build environment uses e.g.:
+        // -- ... / 020.sos-components.v2.7.centos8 / setup-helper / setup-helper-h2 /...
+        Path dir = currentProjectGeneratedSources.getParent();
+        while (dir != null) {
+            Path sp = dir.resolve(sourceProject);
+            if (Files.exists(sp)) {
+                return sp;
+            }
+            dir = dir.getParent();
         }
-        mainProjectDir = mainProjectDir.getParent();
-        if (mainProjectDir == null) {
-            throw new Exception("[currentProject=" + currentProject + "][2]parent is null");
-        }
-
-        Path sourceProjectDir = mainProjectDir.resolve(sourceProject);
-        if (!Files.exists(sourceProjectDir)) {
-            throw new Exception("[source][" + sourceProjectDir + "]not found");
-        }
-        return sourceProjectDir;
+        throw new Exception("[currentProjectGeneratedSources=" + currentProjectGeneratedSources + "][sourceProject=" + sourceProject
+                + "]sourceProject cannot be found in any parent path of currentProjectGeneratedSources");
     }
 
-    public static Path getTargetDir(Path currentProject) throws Exception {
-        Path dir = currentProject.resolve("src/main/java/com");
-        SOSPath.deleteIfExists(dir);
-        return dir;
+    private static void prepareTarget(Path dir) throws Exception {
+        SOSPath.cleanupDirectory(dir);
+        Files.createDirectories(dir);
     }
 
     private static void processDBItemFile(Path sourceBase, Path sourcePath, Path targetBase) {
@@ -74,6 +75,8 @@ public class Executor {
                 Path targetPath = targetBase.resolve(sourceBase.relativize(sourcePath));
                 Files.createDirectories(targetPath.getParent());
                 Files.write(targetPath, content.getBytes());
+
+                System.out.println("[processed]" + targetPath);
             }
         } catch (Throwable e) {
             new RuntimeException(e);
