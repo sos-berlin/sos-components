b46df4238253284c5ab575ef40824025f5f8142e
SOS-RE <robert.ehrlich@sos-berlin.com>
2024-12-12 16:33:53 +0100

    DBItemInventoryDependency - @Proxy(lazy = false) added

--- sos-joc/sos-joc-db/src/main/java/com/sos/joc/db/inventory/DBItemInventoryDependency.java
+++ sos-joc/sos-joc-db/src/main/java/com/sos/joc/db/inventory/DBItemInventoryDependency.java
@@ -1,5 +1,6 @@
 package com.sos.joc.db.inventory;
 
+import org.hibernate.annotations.Proxy;
 import org.hibernate.type.NumericBooleanConverter;
 
 import com.sos.joc.db.DBItem;
@@ -13,8 +14,10 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 
+@SuppressWarnings("deprecation")
 @Entity
 @Table(name = DBLayer.TABLE_INV_DEPENDENCIES)
+@Proxy(lazy = false)
 public class DBItemInventoryDependency extends DBItem {
 
     private static final long serialVersionUID = 1L;
@@ -40,13 +43,14 @@ public class DBItemInventoryDependency extends DBItem {
     @Column(name = "[CONTROLLER_ID]", nullable = false)
     private String controllerId;
 
-
     public Integer getDependencyType() {
         return dependencyType;
     }
+
     public void setDependencyType(Integer type) {
         dependencyType = type;
     }
+
     @Transient
     public ConfigurationType getDependencyTypeAsEnum() {
         try {
@@ -55,44 +59,50 @@ public class DBItemInventoryDependency extends DBItem {
             return null;
         }
     }
+
     @Transient
     public void setDependencyType(ConfigurationType type) {
         setDependencyType(type == null ? null : type.intValue());
     }
-    
+
     public Long getInvId() {
         return invId;
     }
+
     public void setInvId(Long invId) {
         this.invId = invId;
     }
-    
+
     public Long getInvDependencyId() {
         return invDependencyId;
     }
+
     public void setInvDependencyId(Long invDependencyId) {
         this.invDependencyId = invDependencyId;
     }
-    
+
     public Long getDepDependencyId() {
         return depDependencyId;
     }
+
     public void setDepDependencyId(Long depDependencyId) {
         this.depDependencyId = depDependencyId;
     }
-    
+
     public String getControllerId() {
         return controllerId;
     }
+
     public void setControllerId(String controllerId) {
         this.controllerId = controllerId;
     }
-    
+
     public Boolean getPublished() {
         return published;
     }
+
     public void setPublished(Boolean published) {
-        if(published == null) {
+        if (published == null) {
             this.published = false;
         }
         this.published = published;
