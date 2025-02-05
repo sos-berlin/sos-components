c9bbaa5441fcd6fd715e705af21de2fd33940604
oh <oliver.haufe@sos-berlin.com>
2025-02-05 16:24:10 +0100

    JOC-1981

--- sos-webservices/sos-webservices-json/src/main/java/com/sos/joc/model/KioskViewProperties.java
+++ sos-webservices/sos-webservices-json/src/main/java/com/sos/joc/model/KioskViewProperties.java
@@ -1,15 +1,13 @@
 
 package com.sos.joc.model;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
@@ -20,16 +18,8 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
 })
 public class KioskViewProperties {
 
-
     @JsonIgnore
     private Map<String, Integer> additionalProperties = new LinkedHashMap<String, Integer>();
-
-    /**
-     * No args constructor for use in serialization
-     * 
-     */
-    public KioskViewProperties() {
-    }
 
     @JsonAnyGetter
     public Map<String, Integer> getAdditionalProperties() {
@@ -40,15 +30,15 @@ public class KioskViewProperties {
     public void setAdditionalProperty(String name, Integer value) {
         this.additionalProperties.put(name, value);
     }
-    
+
     @Override
     public String toString() {
-        return new ToStringBuilder(this).toString();
+        return new ToStringBuilder(this).append("additionalProperties", additionalProperties).toString();
     }
 
     @Override
     public int hashCode() {
-        return new HashCodeBuilder().toHashCode();
+        return new HashCodeBuilder().append(additionalProperties).toHashCode();
     }
 
     @Override
