6711ebf815dc1e7effa5b3b53e79b4529410d430
oh <oliver.haufe@sos-berlin.com>
2025-04-24 16:12:11 +0200

    new Controller API

--- sos-webservices/sos-webservices-joc-global/src/main/java/com/sos/joc/classes/board/PlanSchemas.java
+++ sos-webservices/sos-webservices-joc-global/src/main/java/com/sos/joc/classes/board/PlanSchemas.java
@@ -5,7 +5,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -38,7 +37,7 @@ public class PlanSchemas {
 
         {
             put(DailyPlanPlanSchemaId, JPlanSchema.of(PlanSchemaId.of(DailyPlanPlanSchemaId), 
-                    Optional.of(JExprFunction.apply("(day) => $day < $" + DailyPlanThresholdKey).asScala()),
+                    JExprFunction.apply("(day) => $day >= $" + DailyPlanThresholdKey).asScala(),
                     new HashMap<>(Collections.singletonMap(DailyPlanThresholdKey, StringValue.empty()))));
         }
     });
@@ -88,7 +87,7 @@ public class PlanSchemas {
     }
     
     private static Map<Boolean, List<JPlanSchema>> getGroupedPlanSchemas(JControllerState currentState) {
-        Function<JPlanSchema, JPlanSchema> cloneWithoutRevision = ps -> JPlanSchema.of(ps.id(), ps.planIsClosedFunction(), ps
+        Function<JPlanSchema, JPlanSchema> cloneWithoutRevision = ps -> JPlanSchema.of(ps.id(), ps.unknownPlanIsOpenFunction(), ps
                 .namedValues());
         Set<JPlanSchema> knownPlanSchemas = currentState.idToPlanSchemaState().values().stream().filter(p -> !p.asScala().isGlobal()).map(
                 JPlanSchemaState::item).map(cloneWithoutRevision).collect(Collectors.toSet());
