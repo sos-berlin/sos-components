9827437411bba75e9ecfe74e4a5f8393e1f74872
sp <sp@sos-berlin.com>
2026-01-23 10:50:36 +0100

    ApiExecutorTest: set Jobarguments to required=false

--- sos-js7/sos-js7-job/src/test/java/com/sos/js7/job/jocapi/helper/TestApiExecutorOIDCJobArguments.java
+++ sos-js7/sos-js7-job/src/test/java/com/sos/js7/job/jocapi/helper/TestApiExecutorOIDCJobArguments.java
@@ -7,13 +7,13 @@ public class TestApiExecutorOIDCJobArguments extends JobArguments {
 
     private JobArgument<String> apiURL = new JobArgument<String>("api_url", true);
     private JobArgument<String> body = new JobArgument<String>("body", true);
-    private JobArgument<String> clientId = new JobArgument<String>("clientId", true);;
-    private JobArgument<String> clientSecret = new JobArgument<String>("clientSecret", true);;
-    private JobArgument<String> identityService = new JobArgument<String>("identityService", true);;
-    private JobArgument<String> issuer = new JobArgument<String>("issuer", true);
-    private JobArgument<String> oidcTrustStorePath = new JobArgument<String>("oidcTrustStorePath", true);
-    private JobArgument<String> oidcTrustStorePasswd = new JobArgument<String>("oidcTrustStorePasswd", true);
-    private JobArgument<String> oidcTrustStoreType = new JobArgument<String>("oidcTrustStoreType", true);
+    private JobArgument<String> clientId = new JobArgument<String>("clientId", false);
+    private JobArgument<String> clientSecret = new JobArgument<String>("clientSecret", false);
+    private JobArgument<String> identityService = new JobArgument<String>("identityService", false);
+    private JobArgument<String> issuer = new JobArgument<String>("issuer", false);
+    private JobArgument<String> oidcTrustStorePath = new JobArgument<String>("oidcTrustStorePath", false);
+    private JobArgument<String> oidcTrustStorePasswd = new JobArgument<String>("oidcTrustStorePasswd", false);
+    private JobArgument<String> oidcTrustStoreType = new JobArgument<String>("oidcTrustStoreType", false);
 
     public JobArgument<String> getApiURL() {
         return apiURL;
