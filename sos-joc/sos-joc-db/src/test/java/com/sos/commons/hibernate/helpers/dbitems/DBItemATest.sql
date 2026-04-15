c391ae99e58d8b7e73017cc453b599e3b4b9cdf4
SOS-RE <robert.ehrlich@sos-berlin.com>
2026-04-15 09:48:01 +0200

    JOC-2205 - preparation - junit test

---
+++ sos-joc/sos-joc-db/src/test/java/com/sos/commons/hibernate/helpers/dbitems/DBItemATest.sql
@@ -0,0 +1,41 @@
+######################################################################################################
+# MySQL
+######################################################################################################
+set SQL_MODE=ANSI_QUOTES;
+CREATE TABLE IF NOT EXISTS A_TEST(
+    "ID"                        BIGINT(20)      UNSIGNED    NOT NULL    AUTO_INCREMENT,
+    "NAME"                      VARCHAR(255)                NOT NULL, 
+    "CREATED"                   DATETIME                    NOT NULL,
+    "MODIFIED"                  DATETIME                    NOT NULL,
+    PRIMARY KEY ("ID")
+) ENGINE=InnoDB;
+
+
+
+######################################################################################################
+# ORACLE
+######################################################################################################
+DECLARE table_exist number;
+BEGIN
+SELECT COUNT(*) INTO table_exist FROM USER_TABLES WHERE "TABLE_NAME"='A_TEST';
+    IF (table_exist = 0) THEN
+        EXECUTE IMMEDIATE   'CREATE TABLE A_TEST (
+                                "ID"                        NUMBER(19)          NOT NULL,
+                                "NAME"                      NVARCHAR2(255)      NOT NULL, 
+                                "CREATED"                   DATE                NOT NULL,
+                                "MODIFIED"                  DATE                NOT NULL,
+                                PRIMARY KEY ("ID")
+                            )';
+        DECLARE sequence_exist number; 
+        BEGIN 
+            SELECT COUNT(*) INTO sequence_exist FROM USER_SEQUENCES WHERE "SEQUENCE_NAME"='SEQ_A_TEST'; 
+            IF (sequence_exist = 0) THEN 
+                EXECUTE IMMEDIATE   'CREATE SEQUENCE SEQ_A_TEST
+                                        INCREMENT BY 1
+                                        START WITH 1
+                                        MAXVALUE 9999999999999999999
+                                        MINVALUE 1 CYCLE';          
+            END IF; 
+        END;
+    END IF; 
+END;
