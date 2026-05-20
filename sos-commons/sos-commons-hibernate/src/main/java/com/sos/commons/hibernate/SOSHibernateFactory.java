aaa904e59595cb09cba883d74437377d445b701d
SOS-RE <robert.ehrlich@sos-berlin.com>
2026-05-20 15:43:10 +0200

    JOC-2230

--- sos-commons/sos-commons-hibernate/src/main/java/com/sos/commons/hibernate/SOSHibernateFactory.java
+++ sos-commons/sos-commons-hibernate/src/main/java/com/sos/commons/hibernate/SOSHibernateFactory.java
@@ -360,7 +360,7 @@ public class SOSHibernateFactory implements Serializable {
             configuration.addSqlFunction(SOSHibernateJsonExists.NAME, new SOSHibernateJsonExists(this));
             configuration.addSqlFunction(SOSHibernateRegexp.NAME, new SOSHibernateRegexp(this));
             configuration.addSqlFunction(SOSHibernateSecondsDiff.NAME, new SOSHibernateSecondsDiff(this));
-            configuration.addSqlFunction(SOSHibernateLike.NAME, new SOSHibernateLike());
+            configuration.addSqlFunction(SOSHibernateLike.NAME, new SOSHibernateLike(this));
         }
     }
 
