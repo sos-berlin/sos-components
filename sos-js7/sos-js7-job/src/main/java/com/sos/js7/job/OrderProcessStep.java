16e887dc5a3629cd996313af90cdf73408d47c0c
SOS-RE <robert.ehrlich@sos-berlin.com>
2026-07-17 11:58:25 +0200

    Job API - failed return code fix for failed(final String msg, Throwable e) - use outcome returnCode instead of default 99

--- sos-js7/sos-js7-job/src/main/java/com/sos/js7/job/OrderProcessStep.java
+++ sos-js7/sos-js7-job/src/main/java/com/sos/js7/job/OrderProcessStep.java
@@ -1031,8 +1031,8 @@ public class OrderProcessStep<A extends JobArguments> {
 
         logger.failed2slf4j(getStepInfo(), eToString, ex);
         logger.error(logger.throwable2String(fm, ex));
-        return JOutcome.failed(getJOutcomeFailed(fm, ex), mapProcessResult(getOutcomeVariables(), getReturnCodeFailed(
-                JobHelper.DEFAULT_RETURN_CODE_FAILED)));
+        return JOutcome.failed(getJOutcomeFailed(fm, ex), mapProcessResult(getOutcomeVariables(), getReturnCodeFailed(getReturnCodeSucceeded(outcome
+                .getReturnCode()))));
     }
 
     protected void checkAndLogParameterization(List<JobArgumentException> exceptions, String mockMessage) throws Exception {
@@ -1411,7 +1411,6 @@ public class OrderProcessStep<A extends JobArguments> {
         return JOutcome.failed(fm, mapProcessResult(getOutcomeVariables(), getReturnCodeFailed(outcome.getReturnCode())));
     }
 
-        return JOutcome.failed(getJOutcomeFailed(fm, ex), mapProcessResult(getOutcomeVariables(), getReturnCodeFailed(outcome.getReturnCode())));
     private String getJOutcomeFailed(final String msg, Throwable e) {
         if (e == null) {
             return msg;
