06432c280ad45aaebe5eebfa66ba77c185e95c2a
oh <oliver.haufe@sos-berlin.com>
2026-09-04 13:32:51 +0200

    new controller api

--- sos-webservices/sos-webservices-joc-global/src/test/java/com/sos/joc/classes/proxy/LogTest.java
+++ sos-webservices/sos-webservices-joc-global/src/test/java/com/sos/joc/classes/proxy/LogTest.java
@@ -6,12 +6,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.NoSuchElementException;
 import java.util.OptionalLong;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 import org.junit.AfterClass;
 import org.junit.Assert;
@@ -30,8 +26,6 @@ import js7.base.log.reader.LogLineKey;
 import js7.data.node.Js7ServerId;
 import js7.proxy.javaapi.JControllerProxy;
 import js7.proxy.javaapi.log.JLogSelection;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.SignalType;
 import reactor.core.scheduler.Schedulers;
 
 @Ignore
@@ -54,18 +48,6 @@ public class LogTest {
         Globals.httpConnectionTimeout = connectionTimeOut;
         Proxies.closeAll();
     }
-    
-    public void fluxDoOnComplete() {
-        LOGGER.info("OnComplete");
-    }
-    
-    public void fluxDoFinally(SignalType type) {
-        LOGGER.info("OnFinally: " + type.toString());
-    }
-    
-    public void fluxDoOnError(Throwable t) {
-        LOGGER.error("OnError", t);
-    }
 
     @Test
     public void testLog6() {
@@ -75,62 +57,20 @@ public class LogTest {
             
             ZoneId zoneId = ZoneId.of(proxy.currentState().asScala().controllerMetaState().timezone().string());
             LOGGER.info("Controller ZoneId: " + zoneId.getId());
-            //Instant instant = Instant.now().minusSeconds(3600);
-            //Instant instant = LocalDate.now().atStartOfDay().toInstant(ZoneOffset.UTC);
-            //Flux<List<KeyedLogLine>> flux = proxy.keyedLogLineFlux(LogLevel.debug(), instant, 100l);
-            //LogLineKey.
             
             JLogSelection selection = JLogSelection.empty().withLineLimit(OptionalLong.of(10l));
-            Flux<List<KeyedLogLine>> flux = proxy.keyedLogLineFlux(Js7ServerId.primaryController, LogLevel.debug(), Instant.parse(
-                    "2026-03-03T17:35:00Z"), selection);
-            // Error handling and completion
-            flux = flux.doOnError(this::fluxDoOnError);
-            flux = flux.doOnComplete(this::fluxDoOnComplete);
-            //flux = flux.doOnCancel(this::fluxDoOnCancel);
-            flux = flux.doFinally(this::fluxDoFinally);
-            Map<String, Object> response = new HashMap<>();
             List<String> loglines = new ArrayList<>();
-//            LogLineKey llk = flux.publishOn(Schedulers.fromExecutor(ForkJoinPool.commonPool())).doOnNext(keyedLogLines -> {
-//                //System.out.println(keyedLogLines);
-//                keyedLogLines.stream().map(KeyedLogLine::line).forEach(LOGGER::info);
-//                //loglines.addAll(keyedLogLines.stream().map(KeyedLogLine::line).toList());
-//            }).reduce(LogLineKey.parse("0/0").toOption().get(), (a, lines) -> a = lines.get(lines.size() - 1).key()).toFuture().get();
             
-            flux = proxy.keyedLogLineFlux(Js7ServerId.primaryController, LogLevel.debug(), Instant.parse("2026-03-03T17:35:00Z"), selection);
-            LogLineKey llk2 = flux.publishOn(Schedulers.fromExecutor(ForkJoinPool.commonPool())).doOnNext(keyedLogLines -> {
-                //System.out.println(keyedLogLines);
-                //keyedLogLines.stream().map(KeyedLogLine::line).forEach(LOGGER::info);
-                loglines.addAll(keyedLogLines.stream().map(KeyedLogLine::line).toList());
-            }).last().map(lines -> lines.get(lines.size() - 1).key()).toFuture().get();
-            
-//            LOGGER.info("loglineKey instant: " + llk.instant().toString());
-//            LOGGER.info("loglineKey position: " + llk.position());
-            LOGGER.info("loglineKey fileinstant: " + llk2.fileInstant().toString());
-            LOGGER.info("loglineKey position: " + llk2.position());
-            
-            LogLineKey llkTest = llk2; //LogLineKey.parse("0/150041").toOption().get();
-            flux = proxy.keyedLogLineFlux(Js7ServerId.primaryController, LogLevel.debug(), llkTest, selection);
-            LogLineKey llk3;
-            try {
-                llk3 = flux.publishOn(Schedulers.fromExecutor(ForkJoinPool.commonPool())).doOnNext(keyedLogLines -> {
-                    //System.out.println(keyedLogLines);
-                    //keyedLogLines.stream().map(KeyedLogLine::line).forEach(LOGGER::info);
-                    loglines.addAll(keyedLogLines.stream().map(KeyedLogLine::line).toList());
-                }).last().map(lines -> lines.get(lines.size() - 1).key()).toFuture().get(57, TimeUnit.SECONDS);
-            } catch (ExecutionException e) {
-                if (e.getCause() != null && e.getCause() instanceof NoSuchElementException) {
-                    LOGGER.info("Empty flux");
-                }
-                llk3 = llkTest;
-            } catch (TimeoutException e) {
-                LOGGER.info("Empty flux");
-                llk3 = llkTest;
-            }
-            //.reduce(llkTest, (a, lines) -> a = lines.get(lines.size() - 1).key()).toFuture().get();
+            LogLineKey llk = proxy.engineLog(Js7ServerId.primaryController, LogLevel.debug()).flatMap(eLog -> eLog.keyedLogLineFlux(Instant.parse(
+                    "2026-03-03T17:35:00Z"), selection).publishOn(Schedulers.fromExecutor(ForkJoinPool.commonPool())).doOnNext(keyedLogLines -> {
+                        //System.out.println(keyedLogLines);
+                        //keyedLogLines.stream().map(KeyedLogLine::line).forEach(LOGGER::info);
+                        loglines.addAll(keyedLogLines.stream().map(KeyedLogLine::line).toList());
+                    })).last().map(lines -> lines.get(lines.size() - 1).key()).toFuture().get();
+            LOGGER.info("loglineKey fileinstant: " + llk.fileInstant().toString());
+            LOGGER.info("loglineKey position: " + llk.position());
+            Map<String, Object> response = new HashMap<>();
             response.put("loglines", loglines);
-            response.put("loglineKey", llk3.toString());
-            LOGGER.info("loglineKey fileinstant: " + llk3.fileInstant().toString());
-            LOGGER.info("loglineKey position: " + llk3.position());
             
             LOGGER.info(Globals.prettyPrintObjectMapper.writeValueAsString(response));
             
